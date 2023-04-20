c69cafa1740087b480414fdf7937e95911d77620
Doniyor Murodkulov <45174298+donik1998@users.noreply.github.com>
2023-04-20 18:23:52 +0500

    fixing broken screen
    we are having const border radius, but there is no context in compile time so screen util does not know anything about the screen size yet

--- lib/foundation/ui_kit_border_radius.dart
+++ lib/foundation/ui_kit_border_radius.dart
@@ -4,20 +4,21 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class BorderRadiusFoundation {
   BorderRadiusFoundation._();
 
-  static final all24 = const BorderRadius.all(Radius.circular(24)).r;
+  static final all24 = const BorderRadius.all(Radius.circular(24));
+  static final all40 = const BorderRadius.all(Radius.circular(40));
   static final onlyTop40 = const BorderRadius.only(
     topLeft: Radius.circular(40),
     topRight: Radius.circular(40),
-  ).r;
+  );
 
   static final onlyTop24 = const BorderRadius.only(
     topLeft: Radius.circular(24),
     topRight: Radius.circular(24),
-  ).r;
+  );
 
   static final onlyBottom24 = const BorderRadius.only(
     bottomLeft: Radius.circular(24),
     bottomRight: Radius.circular(24),
   ).r;
-  static final max = const BorderRadius.all(Radius.circular(9999)).r;
+  static final max = const BorderRadius.all(Radius.circular(9999));
 }
