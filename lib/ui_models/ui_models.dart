0620c9103d7fdad446a37591603ad8a221fea0b1
Doniyor Murodkulov <45174298+donik1998@users.noreply.github.com>
2023-04-21 19:22:16 +0500

    place description done

--- lib/ui_models/ui_models.dart
+++ lib/ui_models/ui_models.dart
@@ -1,6 +1,7 @@
 /// This file exports all the UI models in the UI Kit.
 
 export 'places/place.dart';
+export 'places/place_description_item.dart';
 export 'profile/profile.dart';
 export 'profile/profile_post.dart';
 export 'profile/profile_story.dart';
