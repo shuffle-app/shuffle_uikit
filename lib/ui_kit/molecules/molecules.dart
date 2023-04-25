bc4aa1023aa9428b56d86c699c37623c5d6e2911
Doniyor Murodkulov <45174298+donik1998@users.noreply.github.com>
2023-04-26 04:42:38 +0500

    sync with master

--- lib/ui_kit/molecules/molecules.dart
+++ lib/ui_kit/molecules/molecules.dart
@@ -1,8 +1,8 @@
 /// This file exports all molecules in the molecules folder
 
 export 'app_bar/custom_app_bar.dart';
-export 'place/title_with_avatar.dart';
 export 'place/place_description_grid.dart';
+export 'place/title_with_avatar.dart';
 export 'profile/profile_attitude_tab.dart';
 export 'profile/profile_highlights_card.dart';
 export 'profile/profile_info.dart';
