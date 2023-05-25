9c7535d4e19384e7bb200f02bb04b794cd80e651
naveenpardeep <naveensheoran1000@gmail.com>
2023-05-25 08:21:38 +0300

    usecase for UiKitTagWidget

---
+++ example/test/lib/ui_kit/molecules/place/uikitTagsWidget_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  group('UiKitTagsWidget', () {
+    ThemeData theme = UiKitThemeFoundation.defaultTheme;
+
+    testGoldens('UiKitTagsWidget', (tester) async {
+      debugDisableShadows = false;
+
+      final builder = GoldenBuilder.grid(
+        columns: 1,
+        widthToHeightRatio: 1,
+        bgColor: theme.scaffoldBackgroundColor,
+        wrap: (child) => child,
+      )..addScenario(
+          'UiKitTagsWidget',
+          UiKitTagWidget(
+            title: 'Club',
+            icon: Assets.images.svg.cocktail.path,
+          ),
+        );
+
+      await tester.pumpWidgetBuilder(ScreenUtilInit(
+          designSize: const Size(414, 800),
+          child: builder.build(),
+          builder: (BuildContext context, Widget? child) =>
+              StatefulBuilder(builder: (context, setState) {
+                return UiKitTheme(
+                    onThemeUpdated: (theme) => setState(() => theme = theme),
+                    child: Material(child: Theme(data: theme, child: child!)));
+              })));
+
+      await screenMatchesGolden(tester, 'UiKitTagsWidget_width414',
+          autoHeight: true);
+    });
+  });
+}
