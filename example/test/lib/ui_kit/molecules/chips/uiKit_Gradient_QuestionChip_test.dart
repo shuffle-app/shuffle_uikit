3e359681dc8d65d9d86975a689cdef3d53dafd89
naveenpardeep <naveensheoran1000@gmail.com>
2023-06-04 09:33:36 +0300

    golden test on UiKitGradientQuestionChip

---
+++ example/test/lib/ui_kit/molecules/chips/uiKit_Gradient_QuestionChip_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  group('UiKit Gradient QuestionChip', () {
+    ThemeData theme = UiKitThemeFoundation.defaultTheme;
+
+    testGoldens('UiKit Gradient QuestionChip', (tester) async {
+      debugDisableShadows = false;
+
+      final builder = GoldenBuilder.column(
+        bgColor: theme.scaffoldBackgroundColor,
+        wrap: (child) => child,
+      )..addScenario(
+          'Tell us more',
+          const UiKitGradientQuestionChip(
+            message: 'Tell us more Tell us more Tell us more',
+          ),
+        );
+
+      await tester.pumpWidgetBuilder(ScreenUtilInit(
+          designSize: const Size(414, 568),
+          child: builder.build(),
+          builder: (BuildContext context, Widget? child) =>
+              StatefulBuilder(builder: (context, setState) {
+                return UiKitTheme(
+                    onThemeUpdated: (theme) => setState(() => theme = theme),
+                    child: Material(child: Theme(data: theme, child: child!)));
+              })));
+
+      await screenMatchesGolden(tester, 'UiKitGradientQuestionChip_more_content',
+          autoHeight: true);
+    });
+  });
+}
