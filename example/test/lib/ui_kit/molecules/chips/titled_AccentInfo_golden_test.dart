324c36cdebd43a032a3290f33c8d0080a99feac4
naveenpardeep <naveensheoran1000@gmail.com>
2023-06-06 12:14:19 +0300

    golden test on TitledAccentInfo

---
+++ example/test/lib/ui_kit/molecules/chips/titled_AccentInfo_golden_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  group('TitledAccentInfo', () {
+    ThemeData theme = UiKitThemeFoundation.defaultTheme;
+
+    testGoldens('TitledAccentInfo', (tester) async {
+      debugDisableShadows = false;
+
+      final builder = GoldenBuilder.column(
+        bgColor: theme.scaffoldBackgroundColor,
+        wrap: (child) => child,
+      )..addScenario(
+          'TitledAccentInfo',
+          TitledAccentInfo(
+                title: 'Don’t miss it',
+                info: '11:00 am, November 20    golden was used in the test "TitledAccentInfo TitledAccentInfo"',
+              ),
+        );
+
+      await tester.pumpWidgetBuilder(ScreenUtilInit(
+          designSize: const Size(414, 568),
+          child: builder.build(),
+          builder: (BuildContext context, Widget? child) =>
+              StatefulBuilder(builder: (context, setState) {
+                return UiKitTheme(
+                    onThemeUpdated: (theme) => setState(() => theme = theme),
+                    child: Material(child: Theme(data: theme, child: child!)));
+              })));
+
+      await screenMatchesGolden(tester, 'TitledAccentInfo_width414_more_content',
+          autoHeight: true);
+    });
+  });
+}
