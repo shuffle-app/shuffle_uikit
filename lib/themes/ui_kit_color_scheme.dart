5e4d5375ec8dc1ceab45f5746cb5686097e896ad
Roman Prudchenko <romanprudchen@gmail.com>
2023-09-08 14:49:26 +0300

    feat: added new ui_kit_color_scheme class

---
+++ lib/themes/ui_kit_color_scheme.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:shuffle_uikit/foundation/shuffle_ui_kit_foundation.dart';
+
+class UiKitColorScheme extends ColorScheme {
+  final Color surface1;
+  final Color surface2;
+  final Color surface3;
+  final Color surface4;
+  final Color surface5;
+  final Color uiPrimary;
+  final Color darkNeutral100 = ColorsFoundation.darkNeutral100;
+  final Color darkNeutral300 = ColorsFoundation.darkNeutral300;
+  final Color darkNeutral400 = ColorsFoundation.darkNeutral400;
+  final Color darkNeutral500 = ColorsFoundation.darkNeutral500;
+  final Color darkNeutral600 = ColorsFoundation.darkNeutral600;
+  final Color darkNeutral900 = ColorsFoundation.darkNeutral900;
+
+  @override
+  final Color inversePrimary;
+
+  const UiKitColorScheme.light({
+    this.surface1 = ColorsFoundation.lightSurface1,
+    this.surface2 = ColorsFoundation.lightSurface2,
+    this.surface3 = ColorsFoundation.lightSurface3,
+    this.surface4 = ColorsFoundation.lightSurface4,
+    this.surface5 = ColorsFoundation.lightSurface5,
+    this.uiPrimary = ColorsFoundation.solidLightSurface,
+    this.inversePrimary = ColorsFoundation.solidSurface,
+  }) : super.light(primary: uiPrimary, inversePrimary: inversePrimary);
+
+  const UiKitColorScheme.dark({
+    this.surface1 = ColorsFoundation.surface1,
+    this.surface2 = ColorsFoundation.surface2,
+    this.surface3 = ColorsFoundation.surface3,
+    this.surface4 = ColorsFoundation.surface5,
+    this.surface5 = ColorsFoundation.surface5,
+    this.uiPrimary = ColorsFoundation.solidSurface,
+    this.inversePrimary = ColorsFoundation.solidLightSurface,
+  }) : super.dark(primary: uiPrimary, inversePrimary: inversePrimary);
+}
