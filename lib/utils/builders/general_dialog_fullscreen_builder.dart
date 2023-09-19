6ecc9727bf4168dc893e3eefee2a84f3ba889e3e
Doniyor Murodkulov <45174298+donik1998@users.noreply.github.com>
2023-09-19 16:42:33 +0500

    some changes

--- lib/utils/builders/general_dialog_fullscreen_builder.dart
+++ lib/utils/builders/general_dialog_fullscreen_builder.dart
@@ -1,49 +1,56 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_html_iframe/shims/dart_ui_real.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
 
 showUiKitGeneralFullScreenDialog(BuildContext context, GeneralDialogData data) {
   final Key key = UniqueKey();
   // final Key key=Key(DateTime.now().toString());
 
-  return Navigator.push(
-    context,
-    PageRouteBuilder(
-      opaque: false,
-      barrierDismissible: true,
-      barrierLabel: '',
-      maintainState: true,
-      fullscreenDialog: true,
-      barrierColor: Colors.white.withOpacity(0.07),
-      transitionDuration: const Duration(milliseconds: 250),
-      transitionsBuilder: (context, animation1, animation2, child) {
-        return SlideTransition(
-          position: Tween<Offset>(
-            end: Offset.zero,
-            begin: const Offset(0.0, 1.0),
-          ).animate(animation1),
-          child: BackdropFilter(
-            filter: ImageFilter.blur(
-              sigmaX: 50,
-              sigmaY: 50,
-            ),
-            child: child,
-          ),
-        );
-      },
-      pageBuilder: (
-        BuildContext context,
-        Animation<double> animation1,
-        Animation<double> animation2,
-      ) {
-        return UiKitBottomModalSheet(
-          data: data,
-          startAnimation: animation1,
-          dissmissKey: key,
-        );
-      },
-    ),
-  );
+  // return Navigator.push(
+  //   context,
+  //   PageRouteBuilder(
+  //     opaque: false,
+  //     barrierDismissible: true,
+  //     barrierLabel: '',
+  //     maintainState: false,
+  //     fullscreenDialog: true,
+  //     barrierColor: Colors.white.withOpacity(0.07),
+  //     transitionDuration: const Duration(milliseconds: 250),
+  //     transitionsBuilder: (context, animation1, animation2, child) {
+  //       return SlideTransition(
+  //         position: Tween<Offset>(
+  //           end: Offset.zero,
+  //           begin: const Offset(0.0, 1.0),
+  //         ).animate(animation1),
+  //         child: child,
+  //       );
+  //     },
+  //     pageBuilder: (
+  //       BuildContext context,
+  //       Animation<double> animation1,
+  //       Animation<double> animation2,
+  //     ) {
+  //       return AnimatedBuilder(
+  //         animation: animation1,
+  //         builder: (context, child) {
+  //           return BackdropFilter(
+  //             filter: ImageFilter.blur(
+  //               sigmaX: animation1.value * 50,
+  //               sigmaY: animation1.value * 50,
+  //             ),
+  //             child: child,
+  //           );
+  //         },
+  //         child: UiKitBottomModalSheet(
+  //           data: data,
+  //           startAnimation: animation1,
+  //           dissmissKey: key,
+  //         ),
+  //       );
+  //     },
+  //   ),
+  // );
 
   // return showModalBottomSheet(
   //   barrierLabel: '',
@@ -72,42 +79,45 @@ showUiKitGeneralFullScreenDialog(BuildContext context, GeneralDialogData data) {
   //   },
   // );
 
-  // return showGeneralDialog(
-  //   barrierDismissible: true,
-  //   barrierLabel: '',
-  //   useRootNavigator: data.useRootNavigator,
-  //   barrierColor: Colors.white.withOpacity(0.07),
-  //   context: context,
-  //   transitionDuration: const Duration(milliseconds: 250),
-  //   transitionBuilder: (context, animation1, animation2, child) {
-  //     return BackdropFilter(
-  //       filter: ImageFilter.blur(
-  //         sigmaX: animation1.value * 50,
-  //         sigmaY: animation1.value * 50,
-  //       ),
-  //       child: SlideTransition(
-  //         position: Tween<Offset>(
-  //           end: Offset.zero,
-  //           begin: const Offset(0.0, 1.0),
-  //         ).animate(animation1),
-  //         child: child,
-  //       ),
-  //     );
-  //   },
-  //   pageBuilder: (
-  //     BuildContext context,
-  //     Animation<double> animation1,
-  //     Animation<double> animation2,
-  //   ) {
-  //     return RepaintBoundary(
-  //       child: UiKitBottomModalSheet(
-  //         data: data,
-  //         startAnimation: animation1,
-  //         dissmissKey: key,
-  //       ),
-  //     );
-  //   },
-  // );
+  return showGeneralDialog(
+    barrierDismissible: true,
+    barrierLabel: '',
+    useRootNavigator: data.useRootNavigator,
+    barrierColor: Colors.white.withOpacity(0.07),
+    context: context,
+    transitionDuration: const Duration(milliseconds: 250),
+    transitionBuilder: (context, animation1, animation2, child) {
+      return BackdropFilter(
+        filter: ImageFilter.blur(
+          sigmaX: animation1.value * 50,
+          sigmaY: animation1.value * 50,
+        ),
+        child: AnimatedBuilder(
+          animation: animation1,
+          builder: (context, child) {
+            return SlideTransition(
+              position: Tween<Offset>(
+                end: Offset.zero,
+                begin: const Offset(0.0, 1.0),
+              ).animate(animation1),
+              child: child,
+            );
+          },
+          child: child,
+        ),
+      );
+    },
+    pageBuilder: (
+      BuildContext context,
+      Animation<double> animation1,
+      Animation<double> animation2,
+    ) {
+      return UiKitBottomModalSheet(
+        data: data,
+        dissmissKey: key,
+      );
+    },
+  );
 }
 
 //ignore: no-empty-block
