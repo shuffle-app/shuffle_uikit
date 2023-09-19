136ddd0be0d87fa15d40d59cd2db3035a8b8bb7d
Roman Prudchenko <romanprudchen@gmail.com>
2023-09-19 15:07:34 +0300

    refactor: updated methods locations

--- example/lib/presentation/ui/dialog_testings/dialog_testing_page.dart
+++ example/lib/presentation/ui/dialog_testings/dialog_testing_page.dart
@@ -9,157 +9,139 @@ class DialogTestingPage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: SafeArea(
-          child: Column(
-            children: [
-              OrdinaryButton(
-                text: 'Dialog bottom sheet fullscreen',
-                onPressed: () => showUiKitGeneralFullScreenDialog(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SpacingFoundation.verticalSpace16,
+                OrdinaryButton(
+                  text: 'Dialog bottom sheet fullscreen',
+                  onPressed: () => showUiKitGeneralFullScreenDialog(
+                      context,
+                      GeneralDialogData(
+                          topPadding: 300,
+                          useRootNavigator: false,
+                          child: Column(
+                            children: List.generate(5, (index) => const Placeholder().paddingSymmetric(vertical: 8)),
+                          ),
+                          bottomBar: Center(
+                            child: Container(
+                              width: 100,
+                              height: 56,
+                              color: Colors.red,
+                            ),
+                          ))),
+                ),
+                SpacingFoundation.verticalSpace16,
+                OrdinaryButton(
+                  text: 'Dialog calendar picker',
+                  onPressed: () => showUiKitCalendarDialog(
                     context,
-                    GeneralDialogData(
-                        topPadding: 300,
-                        useRootNavigator: false,
+                  ),
+                ),
+                SpacingFoundation.verticalSpace16,
+                OrdinaryButton(
+                  text: 'Dialog time picker',
+                  onPressed: () => showUiKitTimeDialog(
+                    context,
+                  ),
+                ),
+                SpacingFoundation.verticalSpace16,
+                //почему то работает только так отображение поповера, надо подумать
+                Builder(
+                  builder: (c) => OrdinaryButton(
+                    text: 'Dialog comment',
+                    onPressed: () => showUiKitPopover(c,
+                        title: const Text('Premium account'),
+                        buttonText: 'see more',
+                        description: const Text('Only premium account users can post reactions')),
+                  ),
+                ),
+                SpacingFoundation.verticalSpace16,
+                OrdinaryButton(
+                  text: 'Dialog alert',
+                  onPressed: () => showUiKitAlertDialog(
+                    context,
+                    AlertDialogData(
+                        defaultButtonText: 'text',
+                        title: const Text('You sent an invitation to 2 people.'),
+                        content: const Text('Invitations can be viewed in private messages')),
+                  ),
+                ),
+                SpacingFoundation.verticalSpace16,
+                OrdinaryButton(
+                  text: 'Fullscreen Dialog',
+                  onPressed: () {
+                    showUiKitFullScreenAlertDialog(
+                      context,
+                      child: (_, textStyle) => UiKitHintDialog(
+                        title: 'Fullscreen Dialog',
+                        textStyle: context.uiKitTheme?.boldTextTheme.title2,
+                        subtitle: 'you get exactly what you need',
+                        dismissText: 'OKAY, COOL!',
+                        onDismiss: () => Navigator.pop(_),
+                        hintTiles: [
+                          UiKitIconHintCard(
+                            icon: ImageWidget(
+                              svgAsset: GraphicsFoundation.instance.svg.map,
+                            ),
+                            hint: 'your location',
+                          ),
+                          UiKitIconHintCard(
+                            icon: ImageWidget(
+                              svgAsset: GraphicsFoundation.instance.svg.dart,
+                            ),
+                            hint: 'your interests',
+                          ),
+                          UiKitIconHintCard(
+                            icon: ImageWidget(
+                              svgAsset: GraphicsFoundation.instance.svg.sunClouds,
+                            ),
+                            hint: 'weather around',
+                          ),
+                          UiKitIconHintCard(
+                            icon: ImageWidget(
+                              svgAsset: GraphicsFoundation.instance.svg.smileMood,
+                            ),
+                            hint: 'and other 14 scales',
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+                SpacingFoundation.verticalSpace16,
+                context.button(
+                  data: BaseUiKitButtonData(
+                    text: 'Show modal bottom sheet',
+                    onPressed: () => showModalBottomSheet(
+                      context: context,
+                      builder: (context) => Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                        ),
                         child: Column(
                           children: List.generate(5, (index) => const Placeholder().paddingSymmetric(vertical: 8)),
                         ),
-                        bottomBar: Center(
-                          child: Container(
-                            width: 100,
-                            height: 56,
-                            color: Colors.red,
-                          ),
-                        ))),
-              ),
-              SpacingFoundation.verticalSpace16,
-              OrdinaryButton(
-                text: 'Dialog calendar picker',
-                onPressed: () => showUiKitCalendarDialog(
-                  context,
-                ),
-              ),
-              SpacingFoundation.verticalSpace16,
-              OrdinaryButton(
-                text: 'Dialog time picker',
-                onPressed: () => showUiKitTimeDialog(
-                  context,
-                ),
-              ),
-              SpacingFoundation.verticalSpace16,
-              //почему то работает только так отображение поповера, надо подумать
-              Builder(
-                builder: (c) => OrdinaryButton(
-                  text: 'Dialog comment',
-                  onPressed: () => showUiKitPopover(c,
-                      title: const Text('Premium account'),
-                      buttonText: 'see more',
-                      description: const Text('Only premium account users can post reactions')),
-                ),
-              ),
-              SpacingFoundation.verticalSpace16,
-              OrdinaryButton(
-                text: 'Dialog alert',
-                onPressed: () => showUiKitAlertDialog(
-                  context,
-                  AlertDialogData(
-                      defaultButtonText: 'text',
-                      title: const Text('You sent an invitation to 2 people.'),
-                      content: const Text('Invitations can be viewed in private messages')),
-                ),
-              ),
-              SpacingFoundation.verticalSpace16,
-              OrdinaryButton(
-                text: 'Fullscreen Dialog',
-                onPressed: () {
-                  showUiKitFullScreenAlertDialog(
-                    context,
-                    child: (_, textStyle) => UiKitHintDialog(
-                      title: 'Fullscreen Dialog',
-                      textStyle: context.uiKitTheme?.boldTextTheme.title2,
-                      subtitle: 'you get exactly what you need',
-                      dismissText: 'OKAY, COOL!',
-                      onDismiss: () => Navigator.pop(_),
-                      hintTiles: [
-                        UiKitIconHintCard(
-                          icon: ImageWidget(
-                            svgAsset: GraphicsFoundation.instance.svg.map,
-                          ),
-                          hint: 'your location',
-                        ),
-                        UiKitIconHintCard(
-                          icon: ImageWidget(
-                            svgAsset: GraphicsFoundation.instance.svg.dart,
-                          ),
-                          hint: 'your interests',
-                        ),
-                        UiKitIconHintCard(
-                          icon: ImageWidget(
-                            svgAsset: GraphicsFoundation.instance.svg.sunClouds,
-                          ),
-                          hint: 'weather around',
-                        ),
-                        UiKitIconHintCard(
-                          icon: ImageWidget(
-                            svgAsset: GraphicsFoundation.instance.svg.smileMood,
-                          ),
-                          hint: 'and other 14 scales',
-                        ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-              SpacingFoundation.verticalSpace16,
-              context.button(
-                data: BaseUiKitButtonData(
-                  text: 'Show modal bottom sheet',
-                  onPressed: () => showModalBottomSheet(
-                    context: context,
-                    builder: (context) => Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        children: List.generate(5, (index) => const Placeholder().paddingSymmetric(vertical: 8)),
                       ),
                     ),
                   ),
                 ),
-              ),
-              SpacingFoundation.verticalSpace16,
-              OrdinaryButton(
-                text: 'Notification dialog',
-                onPressed: () => showUiKitAlertDialog(
-                  context,
-                  AlertDialogData(
-                    additionalButton: const SizedBox.shrink(),
-                    content: context.dialogButton(
-                      data: BaseUiKitButtonData(onPressed: () {}, text: 'go to settings'),
-                      dialogButtonType: DialogButtonType.buttonBlack,
-                    ),
-                    defaultButtonText: 'no, thanks',
-                    defaultButtonSmall: false,
-                    defaultButtonOutlined: true,
-                    title: Text(
-                      'To allow access to send push notifications, go to your phone settings',
-                      style: context.uiKitTheme?.boldTextTheme.title2.copyWith(
-                        color: context.uiKitTheme?.colorScheme.primary,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
+                SpacingFoundation.verticalSpace16,
+                OrdinaryButton(
+                  text: 'Geolocation alert',
+                  onPressed: () => showGeolocationPopUp(context, onTap: () {}),
                 ),
-              ),
-            ],
+                SpacingFoundation.verticalSpace16,
+                OrdinaryButton(
+                  text: 'Settings alert',
+                  onPressed: () => showSettingsPopUp(context, onTap: () {}),
+                ),
+                SpacingFoundation.verticalSpace16,
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
-                      // context.button(
-                      //   data: BaseUiKitButtonData(
-                      //     onPressed: () {},
-                      //     text: 'GO TO SETTINGS',
-                      //     fit: ButtonFit.fitWidth,
-                      //   ),
-                      // ),
