874305fa0f322cfa70dbfa90bc6500706d20b932
Roman Prudchenko <romanprudchen@gmail.com>
2023-09-21 17:57:11 +0300

    feat: added timeTo field for UiKitTagsWidget

--- lib/ui_kit/molecules/place/ui_kit_tags_widget.dart
+++ lib/ui_kit/molecules/place/ui_kit_tags_widget.dart
@@ -5,17 +5,20 @@ class UiKitTagsWidget extends StatelessWidget {
   final double? rating;
   final List<UiKitTag> baseTags;
   final List<UiKitTag>? uniqueTags;
+  final TimeOfDay? timeTo;
 
   const UiKitTagsWidget({
     Key? key,
     this.rating,
     required this.baseTags,
     this.uniqueTags,
+    this.timeTo,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final theme = context.uiKitTheme;
+    final isTimeToAvailable = timeTo != null && !(timeTo?.hour == 0 && timeTo?.minute == 0);
 
     return Row(
       mainAxisSize: MainAxisSize.max,
@@ -51,45 +54,74 @@ class UiKitTagsWidget extends StatelessWidget {
             children: [
               SizedBox(
                 height: SpacingFoundation.horizontalSpacing16 + 2,
-                child: ListView.separated(
-                  clipBehavior: Clip.hardEdge,
-                  padding: EdgeInsets.zero,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  primary: false,
-                  itemBuilder: (context, index) {
-                    final tag = baseTags.elementAt(index);
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ListView.separated(
+                        clipBehavior: Clip.hardEdge,
+                        padding: EdgeInsets.zero,
+                        scrollDirection: Axis.horizontal,
+                        primary: false,
+                        itemBuilder: (context, index) {
+                          final tag = baseTags.elementAt(index);
 
-                    return UiKitTagWidget(
-                      title: tag.title,
-                      icon: tag.iconPath,
-                      uniqueTag: false,
-                    );
-                  },
-                  separatorBuilder: (context, index) => SpacingFoundation.horizontalSpace8,
-                  itemCount: baseTags.length,
+                          return UiKitTagWidget(
+                            title: tag.title,
+                            icon: tag.iconPath,
+                            uniqueTag: false,
+                          );
+                        },
+                        separatorBuilder: (context, index) => SpacingFoundation.horizontalSpace8,
+                        itemCount: baseTags.length,
+                      ),
+                    ),
+                    if (isTimeToAvailable) ...[
+                      SpacingFoundation.horizontalSpace16,
+                      Text(
+                        '${timeTo!.hour != 0 ? '${timeTo!.hour} h, ' : ''}${timeTo!.minute != 0 ? '${timeTo!.minute} min' : ''}',
+                        style: theme?.boldTextTheme.caption2Medium.copyWith(
+                          color: theme.colorScheme.darkNeutral900,
+                        ),
+                      ),
+                    ]
+                  ],
                 ),
               ),
               if (uniqueTags != null && uniqueTags!.isNotEmpty) ...[
                 SpacingFoundation.verticalSpace4,
-                SizedBox(
-                  height: SpacingFoundation.horizontalSpacing16 + 2,
-                  child: ListView.separated(
-                    clipBehavior: Clip.hardEdge,
-                    padding: EdgeInsets.zero,
-                    shrinkWrap: true,
-                    scrollDirection: Axis.horizontal,
-                    primary: false,
-                    separatorBuilder: (_, __) => SpacingFoundation.horizontalSpace8,
-                    itemCount: uniqueTags!.length,
-                    itemBuilder: (_, index) {
-                      return UiKitTagWidget(
-                        title: uniqueTags![index].title,
-                        icon: uniqueTags![index].iconPath,
-                        uniqueTag: true,
-                      );
-                    },
-                  ),
+                Row(
+                  children: [
+                    if (isTimeToAvailable) ...[
+                      SpacingFoundation.horizontalSpace16,
+                      Text(
+                        '${timeTo!.hour != 0 ? '${timeTo!.hour} h, ' : ''}${timeTo!.minute != 0 ? '${timeTo!.minute} min' : ''}',
+                        style: theme?.boldTextTheme.caption2Medium.copyWith(
+                          color: theme.colorScheme.darkNeutral900,
+                        ),
+                      ),
+                      SpacingFoundation.horizontalSpace8
+                    ],
+                    Expanded(
+                      child: SizedBox(
+                        height: SpacingFoundation.horizontalSpacing16 + 2,
+                        child: ListView.separated(
+                          clipBehavior: Clip.hardEdge,
+                          padding: EdgeInsets.zero,
+                          scrollDirection: Axis.horizontal,
+                          primary: false,
+                          separatorBuilder: (_, __) => SpacingFoundation.horizontalSpace8,
+                          itemCount: uniqueTags!.length,
+                          itemBuilder: (_, index) {
+                            return UiKitTagWidget(
+                              title: uniqueTags![index].title,
+                              icon: uniqueTags![index].iconPath,
+                              uniqueTag: true,
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 )
               ],
             ],
