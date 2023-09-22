1642707f6403ef8ba3963e88234b969d0c56e316
Roman Prudchenko <romanprudchen@gmail.com>
2023-09-22 12:41:55 +0300

    updated normalizedTi function

--- lib/utils/data_formatters/time_format.dart
+++ lib/utils/data_formatters/time_format.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 String normalizedTi(TimeOfDay? time, {bool showDateName = true}) {
   if (time == null) return 'nn';
 
-  return '${leadingZeros(time.hour)}:${leadingZeros(time.minute)} ${showDateName ? time.period.name : ''}';
+  return '${leadingZeros(time.hour)}:${leadingZeros(time.minute)}${showDateName ? ' ${time.period.name}' : ''}';
 }
 
 String leadingZeros(int number) {
