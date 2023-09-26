e75fdb641dfbcd79041ca19e2a369f62c60e5ded
Roman Prudchenko <romanprudchen@gmail.com>
2023-09-26 13:29:37 +0300

    feat: added new date format logic

--- lib/utils/data_formatters/time_format.dart
+++ lib/utils/data_formatters/time_format.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 String normalizedTi(TimeOfDay? time, {bool showDateName = true}) {
   if (time == null) return 'nn';
@@ -8,4 +9,57 @@ String normalizedTi(TimeOfDay? time, {bool showDateName = true}) {
 
 String leadingZeros(int number) {
   return number.toString().padLeft(2, '0');
+}
+
+String formatDate(
+  DateTime? date,
+  DateTime? dateTo,
+  TimeOfDay? time,
+  TimeOfDay? timeTo,
+  List<String>? weekdays,
+) {
+  String convertedDate = normalizedTi(time, showDateName: false);
+  convertedDate += ' - ${normalizedTi(timeTo, showDateName: false)}, ';
+  if (weekdays != null && weekdays.isNotEmpty) {
+    bool isConsecutive = true;
+    for (int ind = 1; ind < weekdays.length; ind++) {
+      if (weekdays[ind] != _getFollowingDay(weekdays[ind - 1])) {
+        isConsecutive = false;
+        break;
+      }
+    }
+    if (isConsecutive) {
+      convertedDate += '${weekdays.first.substring(0, 3)} - ${weekdays.last.substring(0, 3)}';
+
+      return convertedDate;
+    }
+
+    convertedDate += weekdays.join(', ');
+  } else {
+    convertedDate += date != null ? DateFormat('MMM dd').format(date) : 'nn';
+    convertedDate += dateTo != null ? ' - ${DateFormat('MMM dd, yyyy').format(dateTo)}' : '';
+  }
+
+  return convertedDate;
+}
+
+String _getFollowingDay(String day) {
+  switch (day) {
+    case 'Monday':
+      return 'Tuesday';
+    case 'Tuesday':
+      return 'Wednesday';
+    case 'Wednesday':
+      return 'Thursday';
+    case 'Thursday':
+      return 'Friday';
+    case 'Friday':
+      return 'Saturday';
+    case 'Saturday':
+      return 'Sunday';
+    case 'Sunday':
+      return 'Monday';
+    default:
+      return '';
+  }
 }
