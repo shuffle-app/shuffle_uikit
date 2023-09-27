f7b5fa2266b1f6cb9d4d006987a365b9547cbc89
Roman Prudchenko <romanprudchen@gmail.com>
2023-09-27 11:12:17 +0300

    refactor: updated formatDate function

--- lib/utils/data_formatters/time_format.dart
+++ lib/utils/data_formatters/time_format.dart
@@ -11,55 +11,20 @@ String leadingZeros(int number) {
   return number.toString().padLeft(2, '0');
 }
 
-String formatDate(
-  DateTime? date,
-  DateTime? dateTo,
-  TimeOfDay? time,
-  TimeOfDay? timeTo,
-  List<String>? weekdays,
-) {
+String formatDate(DateTime? date, DateTime? dateTo, TimeOfDay? time, TimeOfDay? timeTo, List<String>? weekdays) {
   String convertedDate = normalizedTi(time, showDateName: false);
   convertedDate += ' - ${normalizedTi(timeTo, showDateName: false)}, ';
   if (weekdays != null && weekdays.isNotEmpty) {
-    bool isConsecutive = true;
-    for (int ind = 1; ind < weekdays.length; ind++) {
-      if (weekdays[ind] != _getFollowingDay(weekdays[ind - 1])) {
-        isConsecutive = false;
-        break;
-      }
-    }
-    if (isConsecutive) {
-      convertedDate += '${weekdays.first} - ${weekdays.last}';
+    final List<String> days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    final int start = days.indexOf(weekdays.first);
+    final int end = days.indexOf(weekdays.last);
+    convertedDate += end - start == weekdays.length - 1 ? '${weekdays.first} - ${weekdays.last}' : weekdays.join(', ');
 
-      return convertedDate;
-    }
-
-    convertedDate += weekdays.join(', ');
+    return convertedDate;
   } else {
     convertedDate += date != null ? DateFormat('MMM dd').format(date) : 'nn';
     convertedDate += dateTo != null ? ' - ${DateFormat('MMM dd, yyyy').format(dateTo)}' : '';
   }
 
   return convertedDate;
-}
-
-String _getFollowingDay(String day) {
-  switch (day) {
-    case 'Mon':
-      return 'Tue';
-    case 'Tue':
-      return 'Wed';
-    case 'Wed':
-      return 'Thu';
-    case 'Thu':
-      return 'Fri';
-    case 'Fri':
-      return 'Sat';
-    case 'Sat':
-      return 'Sun';
-    case 'Sun':
-      return 'Mon';
-    default:
-      return '';
-  }
 }
