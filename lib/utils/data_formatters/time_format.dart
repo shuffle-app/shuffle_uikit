c6df8f2f31b90dbbb62c82d71d67f0b0fa9d70c0
Roman Prudchenko <romanprudchen@gmail.com>
2023-09-29 17:59:39 +0300

    refactor: updated formatDate function

--- lib/utils/data_formatters/time_format.dart
+++ lib/utils/data_formatters/time_format.dart
@@ -11,20 +11,30 @@ String leadingZeros(int number) {
   return number.toString().padLeft(2, '0');
 }
 
-String formatDate(DateTime? date, DateTime? dateTo, TimeOfDay? time, TimeOfDay? timeTo, List<String>? weekdays) {
-  String convertedDate = normalizedTi(time, showDateName: false);
-  convertedDate += ' - ${normalizedTi(timeTo, showDateName: false)}, ';
-  if (weekdays != null && weekdays.isNotEmpty) {
+String? formatDate(DateTime? date, DateTime? dateTo, TimeOfDay? time, TimeOfDay? timeTo, List<String>? wDays) {
+  if (date == null && dateTo == null && time == null && timeTo == null && wDays == null) return null;
+  String convDate = '';
+  if (time != null) {
+    convDate = normalizedTi(time, showDateName: false);
+  }
+  if (timeTo != null) {
+    convDate += '${convDate.isEmpty ? '' : ' - '}${normalizedTi(timeTo, showDateName: false)}';
+  }
+  if (wDays != null && wDays.isNotEmpty) {
     final List<String> days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    final int start = days.indexOf(weekdays.first);
-    final int end = days.indexOf(weekdays.last);
-    convertedDate += end - start == weekdays.length - 1 ? '${weekdays.first} - ${weekdays.last}' : weekdays.join(', ');
+    final int start = days.indexOf(wDays.first);
+    final int end = days.indexOf(wDays.last);
+    convDate += end - start == wDays.length - 1
+        ? '${convDate.isEmpty ? '' : ', '}${wDays.first} - ${wDays.last}'
+        : ', ${wDays.join(', ')}';
 
-    return convertedDate;
+    return convDate;
   } else {
-    convertedDate += date != null ? DateFormat('MMM dd').format(date) : 'nn';
-    convertedDate += dateTo != null ? ' - ${DateFormat('MMM dd, yyyy').format(dateTo)}' : '';
+    convDate += date != null ? '${convDate.isEmpty ? '' : ', '}${DateFormat('MMM dd').format(date)}' : '';
+    if (dateTo != null) {
+      convDate += '${date != null ? ' - ' : convDate.isEmpty ? '' : ', '}${DateFormat('MMM dd, yyyy').format(dateTo)}';
+    }
   }
 
-  return convertedDate;
+  return convDate;
 }
