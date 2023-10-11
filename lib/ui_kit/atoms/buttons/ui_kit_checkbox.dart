6fea3c0c9757e78fa4a214456263826da9ed4d34
romancores <225000@niuitmo.ru>
2023-10-11 11:17:11 -0500

    checkbox fix

--- lib/ui_kit/atoms/buttons/ui_kit_checkbox.dart
+++ lib/ui_kit/atoms/buttons/ui_kit_checkbox.dart
@@ -15,10 +15,9 @@ class UiKitCheckbox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GradientableWidget(
-      active: isActive,
-      gradient: GradientFoundation.attentionCard,
-      child: InkWell(
+    return Material(
+      color: Colors.transparent,
+        child: InkWell(
         onTap: onChanged,
         child: Ink(
           padding: EdgeInsets.all(EdgeInsetsFoundation.all2),
@@ -35,7 +34,10 @@ class UiKitCheckbox extends StatelessWidget {
             duration: const Duration(milliseconds: 200),
             curve: Curves.fastOutSlowIn,
             scale: isActive ? 1.0 : 0,
-            child: ImageWidget(
+            child:GradientableWidget(
+              active: isActive,
+              gradient: GradientFoundation.attentionCard,
+              child:  ImageWidget(
               svgAsset: GraphicsFoundation.instance.svg.check,
               color: Colors.white,
               height: SpacingFoundation.horizontalSpacing12,
@@ -44,6 +46,6 @@ class UiKitCheckbox extends StatelessWidget {
           ),
         ),
       ),
-    );
+    ));
   }
 }
