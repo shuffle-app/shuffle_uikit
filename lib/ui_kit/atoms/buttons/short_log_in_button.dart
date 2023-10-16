98e074343ef92d039b1c7f863e5a4700b612511c
Roman Prudchenko <romanprudchen@gmail.com>
2023-10-16 11:31:18 +0300

    feat: added new ShortLogInButton widget

---
+++ lib/ui_kit/atoms/buttons/short_log_in_button.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/widgets.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+
+class ShortLogInButton extends StatelessWidget {
+  const ShortLogInButton({
+    super.key,
+    required this.svgIcon,
+    required this.title,
+    required this.onTap,
+  });
+
+  final SvgGenImage svgIcon;
+  final String title;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.uiKitTheme;
+    final colorScheme = theme?.colorScheme;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: colorScheme!.surface1,
+          borderRadius: BorderRadiusFoundation.all24,
+        ),
+        child: UiKitCardWrapper(
+          color: colorScheme.surface3,
+          child: Row(
+            children: [
+              ImageWidget(
+                svgAsset: svgIcon,
+                height: 20.w,
+                width: 20.w,
+                fit: BoxFit.cover,
+              ),
+              Expanded(
+                child: Text(
+                  title.toUpperCase(),
+                  style: theme?.boldTextTheme.caption1Medium,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ).paddingSymmetric(
+            horizontal: EdgeInsetsFoundation.horizontal8,
+            vertical: EdgeInsetsFoundation.vertical10,
+          ),
+        ).paddingAll(EdgeInsetsFoundation.all4),
+      ),
+    );
+  }
+}
