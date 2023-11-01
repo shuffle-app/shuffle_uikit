6c048daebfad5650b749ae37353d549970894b54
rel1nce <romanprudchen@gmail.com>
2023-11-01 17:35:05 +0300

    feat: updated localization

--- lib/localization/intl/messages_ar.dart
+++ lib/localization/intl/messages_ar.dart
@@ -28,14 +28,35 @@ class MessageLookup extends MessageLookupByLibrary {
   static String m2(invitedPeopleCount) =>
       "Invites ${invitedPeopleCount} people to";
 
+  static String m3(amountOfPeople) => "NEXT ${amountOfPeople} PEOPLE";
+
+  static String m4(religionCount) =>
+      "Please select at least ${religionCount} religion";
+
+  static String m5(separator) => "Tell us more${separator}about yourself";
+
+  static String m6(amountOfTopDonators, multiplier) =>
+      "Top ${amountOfTopDonators} donators receive x${multiplier} points";
+
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "AllowedCaracters": m0,
+        "And": MessageLookupByLibrary.simpleMessage("And"),
+        "AskForDonations":
+            MessageLookupByLibrary.simpleMessage("Ask for donations"),
+        "AskPeople": MessageLookupByLibrary.simpleMessage("Ask people"),
+        "ByContinuingYouAcceptThe": MessageLookupByLibrary.simpleMessage(
+            "By continuing you accept the"),
         "ChooseOption": MessageLookupByLibrary.simpleMessage("Choose option"),
         "CompanyAnswered":
             MessageLookupByLibrary.simpleMessage("Сompany answered"),
         "Confirm": MessageLookupByLibrary.simpleMessage("Confirm"),
+        "Day": MessageLookupByLibrary.simpleMessage("Day"),
+        "DescribeYourself":
+            MessageLookupByLibrary.simpleMessage("Describe yourself"),
         "Directions": MessageLookupByLibrary.simpleMessage("Directions"),
+        "DontBeAlone":
+            MessageLookupByLibrary.simpleMessage("Don\\\'t be alone"),
         "Email": MessageLookupByLibrary.simpleMessage("Email"),
         "EnterYourEmailAddress":
             MessageLookupByLibrary.simpleMessage("Enter your e-mail address"),
@@ -43,8 +64,13 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("Features enable geolocation"),
         "Follow": MessageLookupByLibrary.simpleMessage("Follow"),
         "Followers": MessageLookupByLibrary.simpleMessage("Followers"),
+        "For": MessageLookupByLibrary.simpleMessage("For"),
         "ForgotPassword":
             MessageLookupByLibrary.simpleMessage("Forgot password"),
+        "FreePlaces": MessageLookupByLibrary.simpleMessage("\\nfree places"),
+        "Gender": MessageLookupByLibrary.simpleMessage("Gender"),
+        "GetCode": MessageLookupByLibrary.simpleMessage("Get code"),
+        "GetStarted": MessageLookupByLibrary.simpleMessage("Get started"),
         "Go": MessageLookupByLibrary.simpleMessage("Go!"),
         "GoToSettings": MessageLookupByLibrary.simpleMessage("Go to settings"),
         "HealthKit": MessageLookupByLibrary.simpleMessage("Health Kit"),
@@ -54,12 +80,23 @@ class MessageLookup extends MessageLookupByLibrary {
             "How much \\\$ do you want to donate?"),
         "IncorrectLoginOrPassword":
             MessageLookupByLibrary.simpleMessage("Incorrect login or password"),
+        "Invite": MessageLookupByLibrary.simpleMessage("Invite"),
         "InviteMore": MessageLookupByLibrary.simpleMessage("Invite more"),
+        "InviteToFavouritePlaces":
+            MessageLookupByLibrary.simpleMessage("Invite to favourite places"),
         "InvitesNPeopleTo": m2,
         "InvitesYouTo": MessageLookupByLibrary.simpleMessage("Invites you to"),
         "Loading": MessageLookupByLibrary.simpleMessage("Loading..."),
+        "LovelyTouchAmazingInteraction": MessageLookupByLibrary.simpleMessage(
+            "Lovely-touch amazing\\ninteraction"),
         "Members": MessageLookupByLibrary.simpleMessage("Members"),
+        "Month": MessageLookupByLibrary.simpleMessage("Month"),
+        "Name": MessageLookupByLibrary.simpleMessage("Name"),
         "NewPlace": MessageLookupByLibrary.simpleMessage("New place"),
+        "Next": MessageLookupByLibrary.simpleMessage("NEXT"),
+        "NextNPeople": m3,
+        "NextWithChevrons": MessageLookupByLibrary.simpleMessage("Next >>>"),
+        "Nickname": MessageLookupByLibrary.simpleMessage("Nickname"),
         "NoHealthKitAvailableOnYourDevice": MessageLookupByLibrary.simpleMessage(
             "No health kit available on your device, so the result will be random"),
         "NoPhotoHereYet":
@@ -68,33 +105,76 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("No preferences chosen"),
         "NoThx": MessageLookupByLibrary.simpleMessage("No, thx"),
         "NotNow": MessageLookupByLibrary.simpleMessage("Not now"),
+        "NowLetsGetToKnowEachOther": MessageLookupByLibrary.simpleMessage(
+            "Now let\\\'s get to know each other"),
         "Off": MessageLookupByLibrary.simpleMessage("Off"),
         "Ok": MessageLookupByLibrary.simpleMessage("Ok"),
         "OkayCool": MessageLookupByLibrary.simpleMessage("Okay, cool!"),
         "Ooops": MessageLookupByLibrary.simpleMessage("Ooops!"),
         "OopsIWillTryAgain":
             MessageLookupByLibrary.simpleMessage("Oops, I\'ll try again"),
+        "PleaseSelectAtLeastNReligion": m4,
+        "PleaseSelectGender":
+            MessageLookupByLibrary.simpleMessage("Please select a gender"),
+        "PleaseSelectOneType":
+            MessageLookupByLibrary.simpleMessage("Please select one type"),
         "PointBalance": MessageLookupByLibrary.simpleMessage("Point Balance"),
         "ResetPassword": MessageLookupByLibrary.simpleMessage("Reset password"),
         "Search": MessageLookupByLibrary.simpleMessage("Search"),
         "SeeMore": MessageLookupByLibrary.simpleMessage("See more"),
+        "SelectDate": MessageLookupByLibrary.simpleMessage("Select date"),
         "SelectLanguage":
             MessageLookupByLibrary.simpleMessage("Select language"),
         "SelectOption": MessageLookupByLibrary.simpleMessage("Select option"),
+        "SelectYourReligions":
+            MessageLookupByLibrary.simpleMessage("Select your religions"),
+        "SelectionOfTheBest":
+            MessageLookupByLibrary.simpleMessage("Selection of the best"),
         "SendIt": MessageLookupByLibrary.simpleMessage("Send it"),
+        "Shuffle": MessageLookupByLibrary.simpleMessage("Shuffle"),
+        "SmartLeisureSelection": MessageLookupByLibrary.simpleMessage(
+            "Smart leisure selection for everyone, everywhere."),
+        "StartToExplore":
+            MessageLookupByLibrary.simpleMessage("Start to explore"),
         "Support": MessageLookupByLibrary.simpleMessage("Support"),
+        "SupportPeopleAroundYou":
+            MessageLookupByLibrary.simpleMessage("Support people around you"),
         "TapIt": MessageLookupByLibrary.simpleMessage("Tap it"),
+        "TapOnceToChoose":
+            MessageLookupByLibrary.simpleMessage("Tap once to choose "),
         "TapToSeeMore": MessageLookupByLibrary.simpleMessage("Tap to see more"),
+        "TapTwiceToMarkYourFavorites": MessageLookupByLibrary.simpleMessage(
+            ". Tap twice to mark your favorites."),
+        "TellUsMoreAboutYourself": m5,
         "ThatsAllForNow":
             MessageLookupByLibrary.simpleMessage("That\'s all for now!"),
+        "TheMoreInfoWeGetTheBetter": MessageLookupByLibrary.simpleMessage(
+            "The more info we get about you, the better"),
+        "ThisIs": MessageLookupByLibrary.simpleMessage("This is "),
+        "ThroughANonAggregatorSystem": MessageLookupByLibrary.simpleMessage(
+            "Through a non-\\naggregator system \\nespecially for you"),
         "ToAccessAll": MessageLookupByLibrary.simpleMessage("To access all"),
         "ToAllowAccessGoToSettings": MessageLookupByLibrary.simpleMessage(
             "To allow access to send push notifications, go to your phone settings"),
+        "TopNDonatorsReceiveXPoints": m6,
+        "TopPlacesRatedBy":
+            MessageLookupByLibrary.simpleMessage("Top places rated\\nby"),
+        "TryYourself": MessageLookupByLibrary.simpleMessage("Try\\nyourself"),
         "UpcomingEvent": MessageLookupByLibrary.simpleMessage("Upcoming Event"),
         "WeWillLetKnowResults": MessageLookupByLibrary.simpleMessage(
             "We will let you know the results"),
         "Weather": MessageLookupByLibrary.simpleMessage("Weather"),
+        "Welcome": MessageLookupByLibrary.simpleMessage("Welcome"),
+        "WhatIs": MessageLookupByLibrary.simpleMessage("What is \\n"),
+        "WhatYouLike": MessageLookupByLibrary.simpleMessage("What you like"),
+        "WillBe": MessageLookupByLibrary.simpleMessage("Will be."),
+        "WithYourPreferences":
+            MessageLookupByLibrary.simpleMessage("With your\\npreferences"),
+        "Year": MessageLookupByLibrary.simpleMessage("Year"),
+        "YourAge": MessageLookupByLibrary.simpleMessage("Your age"),
         "YourClaimHasBeenSent":
-            MessageLookupByLibrary.simpleMessage("Your claim has been sent")
+            MessageLookupByLibrary.simpleMessage("Your claim has been sent"),
+        "YourLeisureSelection":
+            MessageLookupByLibrary.simpleMessage("Your leisure selection")
       };
 }
--- lib/localization/l10n.dart
+++ lib/localization/l10n.dart
@@ -559,6 +559,497 @@ class S {
       args: [],
     );
   }
+
+  /// `Next >>>`
+  String get NextWithChevrons {
+    return Intl.message(
+      'Next >>>',
+      name: 'NextWithChevrons',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Day`
+  String get Day {
+    return Intl.message(
+      'Day',
+      name: 'Day',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Month`
+  String get Month {
+    return Intl.message(
+      'Month',
+      name: 'Month',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Year`
+  String get Year {
+    return Intl.message(
+      'Year',
+      name: 'Year',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `NEXT {amountOfPeople} PEOPLE`
+  String NextNPeople(Object amountOfPeople) {
+    return Intl.message(
+      'NEXT $amountOfPeople PEOPLE',
+      name: 'NextNPeople',
+      desc: '',
+      args: [amountOfPeople],
+    );
+  }
+
+  /// `Ask people`
+  String get AskPeople {
+    return Intl.message(
+      'Ask people',
+      name: 'AskPeople',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Ask for donations`
+  String get AskForDonations {
+    return Intl.message(
+      'Ask for donations',
+      name: 'AskForDonations',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Support people around you`
+  String get SupportPeopleAroundYou {
+    return Intl.message(
+      'Support people around you',
+      name: 'SupportPeopleAroundYou',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Top {amountOfTopDonators} donators receive x{multiplier} points`
+  String TopNDonatorsReceiveXPoints(
+      Object amountOfTopDonators, Object multiplier) {
+    return Intl.message(
+      'Top $amountOfTopDonators donators receive x$multiplier points',
+      name: 'TopNDonatorsReceiveXPoints',
+      desc: '',
+      args: [amountOfTopDonators, multiplier],
+    );
+  }
+
+  /// `Tell us more{separator}about yourself`
+  String TellUsMoreAboutYourself(Object separator) {
+    return Intl.message(
+      'Tell us more${separator}about yourself',
+      name: 'TellUsMoreAboutYourself',
+      desc: '',
+      args: [separator],
+    );
+  }
+
+  /// `Tap once to choose `
+  String get TapOnceToChoose {
+    return Intl.message(
+      'Tap once to choose ',
+      name: 'TapOnceToChoose',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `What you like`
+  String get WhatYouLike {
+    return Intl.message(
+      'What you like',
+      name: 'WhatYouLike',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `. Tap twice to mark your favorites.`
+  String get TapTwiceToMarkYourFavorites {
+    return Intl.message(
+      '. Tap twice to mark your favorites.',
+      name: 'TapTwiceToMarkYourFavorites',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Try\nyourself`
+  String get TryYourself {
+    return Intl.message(
+      'Try\\nyourself',
+      name: 'TryYourself',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Invite to favourite places`
+  String get InviteToFavouritePlaces {
+    return Intl.message(
+      'Invite to favourite places',
+      name: 'InviteToFavouritePlaces',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Invite`
+  String get Invite {
+    return Intl.message(
+      'Invite',
+      name: 'Invite',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Select date`
+  String get SelectDate {
+    return Intl.message(
+      'Select date',
+      name: 'SelectDate',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `NEXT`
+  String get Next {
+    return Intl.message(
+      'NEXT',
+      name: 'Next',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `By continuing you accept the`
+  String get ByContinuingYouAcceptThe {
+    return Intl.message(
+      'By continuing you accept the',
+      name: 'ByContinuingYouAcceptThe',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `And`
+  String get And {
+    return Intl.message(
+      'And',
+      name: 'And',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Get code`
+  String get GetCode {
+    return Intl.message(
+      'Get code',
+      name: 'GetCode',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Start to explore`
+  String get StartToExplore {
+    return Intl.message(
+      'Start to explore',
+      name: 'StartToExplore',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Now let\'s get to know each other`
+  String get NowLetsGetToKnowEachOther {
+    return Intl.message(
+      'Now let\\\'s get to know each other',
+      name: 'NowLetsGetToKnowEachOther',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `The more info we get about you, the better`
+  String get TheMoreInfoWeGetTheBetter {
+    return Intl.message(
+      'The more info we get about you, the better',
+      name: 'TheMoreInfoWeGetTheBetter',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Your leisure selection`
+  String get YourLeisureSelection {
+    return Intl.message(
+      'Your leisure selection',
+      name: 'YourLeisureSelection',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Will be.`
+  String get WillBe {
+    return Intl.message(
+      'Will be.',
+      name: 'WillBe',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Name`
+  String get Name {
+    return Intl.message(
+      'Name',
+      name: 'Name',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Nickname`
+  String get Nickname {
+    return Intl.message(
+      'Nickname',
+      name: 'Nickname',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Describe yourself`
+  String get DescribeYourself {
+    return Intl.message(
+      'Describe yourself',
+      name: 'DescribeYourself',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Select your religions`
+  String get SelectYourReligions {
+    return Intl.message(
+      'Select your religions',
+      name: 'SelectYourReligions',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Your age`
+  String get YourAge {
+    return Intl.message(
+      'Your age',
+      name: 'YourAge',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Gender`
+  String get Gender {
+    return Intl.message(
+      'Gender',
+      name: 'Gender',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Please select a gender`
+  String get PleaseSelectGender {
+    return Intl.message(
+      'Please select a gender',
+      name: 'PleaseSelectGender',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Please select at least {religionCount} religion`
+  String PleaseSelectAtLeastNReligion(Object religionCount) {
+    return Intl.message(
+      'Please select at least $religionCount religion',
+      name: 'PleaseSelectAtLeastNReligion',
+      desc: '',
+      args: [religionCount],
+    );
+  }
+
+  /// `Please select one type`
+  String get PleaseSelectOneType {
+    return Intl.message(
+      'Please select one type',
+      name: 'PleaseSelectOneType',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `This is `
+  String get ThisIs {
+    return Intl.message(
+      'This is ',
+      name: 'ThisIs',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Shuffle`
+  String get Shuffle {
+    return Intl.message(
+      'Shuffle',
+      name: 'Shuffle',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Smart leisure selection for everyone, everywhere.`
+  String get SmartLeisureSelection {
+    return Intl.message(
+      'Smart leisure selection for everyone, everywhere.',
+      name: 'SmartLeisureSelection',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Welcome`
+  String get Welcome {
+    return Intl.message(
+      'Welcome',
+      name: 'Welcome',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Get started`
+  String get GetStarted {
+    return Intl.message(
+      'Get started',
+      name: 'GetStarted',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `What is \n`
+  String get WhatIs {
+    return Intl.message(
+      'What is \\n',
+      name: 'WhatIs',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `For`
+  String get For {
+    return Intl.message(
+      'For',
+      name: 'For',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Through a non-\naggregator system \nespecially for you`
+  String get ThroughANonAggregatorSystem {
+    return Intl.message(
+      'Through a non-\\naggregator system \\nespecially for you',
+      name: 'ThroughANonAggregatorSystem',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `With your\npreferences`
+  String get WithYourPreferences {
+    return Intl.message(
+      'With your\\npreferences',
+      name: 'WithYourPreferences',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Don\'t be alone`
+  String get DontBeAlone {
+    return Intl.message(
+      'Don\\\'t be alone',
+      name: 'DontBeAlone',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Lovely-touch amazing\ninteraction`
+  String get LovelyTouchAmazingInteraction {
+    return Intl.message(
+      'Lovely-touch amazing\\ninteraction',
+      name: 'LovelyTouchAmazingInteraction',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Selection of the best`
+  String get SelectionOfTheBest {
+    return Intl.message(
+      'Selection of the best',
+      name: 'SelectionOfTheBest',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `\nfree places`
+  String get FreePlaces {
+    return Intl.message(
+      '\\nfree places',
+      name: 'FreePlaces',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Top places rated\nby`
+  String get TopPlacesRatedBy {
+    return Intl.message(
+      'Top places rated\\nby',
+      name: 'TopPlacesRatedBy',
+      desc: '',
+      args: [],
+    );
+  }
 }
 
 class AppLocalizationDelegate extends LocalizationsDelegate<S> {
