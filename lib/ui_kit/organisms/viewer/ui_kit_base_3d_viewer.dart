081a5f3d065b32989def2628da9479847143bead
romancores <225000@niuitmo.ru>
2024-03-04 13:15:44 -0500

    SHF-817

--- lib/ui_kit/organisms/viewer/ui_kit_base_3d_viewer.dart
+++ lib/ui_kit/organisms/viewer/ui_kit_base_3d_viewer.dart
@@ -1,31 +1,44 @@
+import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 class UiKitBase3DViewer extends StatelessWidget {
   final String localPath;
+  final Color? backgroundColor;
+  final String? animationName;
+  final bool? autoPlay;
+  final String? environmentImage;
+  final bool? autoRotate;
+  final ValueChanged<WebViewController>? onWebViewCreated;
 
-  const UiKitBase3DViewer({super.key, required this.localPath});
+  const UiKitBase3DViewer(
+      {super.key,
+      required this.localPath,
+      this.backgroundColor,
+      this.animationName,
+      this.autoPlay,
+      this.onWebViewCreated,
+      this.environmentImage,
+      this.autoRotate});
 
   @override
   Widget build(BuildContext context) {
-
     return ModelViewer(
-      // backgroundColor: theme?.colorScheme.background ?? UiKitColors.surface1,
+      onWebViewCreated: onWebViewCreated,
+      animationName: animationName,
+      autoPlay: autoPlay,
+      id: 'model-viewer',
+      backgroundColor: backgroundColor ?? Colors.transparent,
       src: 'file://$localPath',
-      // alt: 'A 3D model of a person',
       ar: true,
+      disableTap: true,
       xrEnvironment: true,
       arPlacement: ArPlacement.floor,
-      // touchAction: TouchAction.panY,
       arModes: const ['scene-viewer', 'webxr', 'quick-look'],
-      autoRotate: true,
-      // iosSrc: 'https://modelviewer.dev/shared-assets/models/Astronaut.usdz',
+      autoRotate: autoRotate,
       disableZoom: true,
-      // shadowSoftness: 4,
-      // shadowIntensity: 2,
-      exposure: 3,
-      environmentImage:
-          'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/i/a57581b5-938b-4dc4-b6af-64d51df7f8d4/d6vv3z5-7593e8e2-bc13-44df-a1d1-0ae5fedb492c.jpg',
+      exposure: 1,
+      environmentImage: environmentImage,
     );
   }
 }
