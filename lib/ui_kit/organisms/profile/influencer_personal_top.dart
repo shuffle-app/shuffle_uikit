4b092fe38cb8ee932b69f34eee3838921352d0b3
Doniyor Murodkulov <45174298+donik1998@users.noreply.github.com>
2024-05-09 21:34:36 +0500

    adding missing files

---
+++ lib/ui_kit/organisms/profile/influencer_personal_top.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+
+class InfluencerPersonalTop extends StatelessWidget {
+  final List<InfluencerTopCategory> categories;
+
+  const InfluencerPersonalTop({
+    Key? key,
+    required this.categories,
+  }) : super(key: key);
+
+  double get contentImageWidth => 0.25.sw;
+
+  double get contentImageHeight => contentImageWidth * 0.75;
+
+  @override
+  Widget build(BuildContext context) {
+    final boldTextTheme = context.uiKitTheme?.boldTextTheme;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          S.current.PersonalTop,
+          style: boldTextTheme?.title1,
+        ),
+        SpacingFoundation.verticalSpace16,
+        ListView.separated(
+          physics: const NeverScrollableScrollPhysics(),
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          separatorBuilder: (context, index) => SpacingFoundation.verticalSpace16,
+          itemBuilder: (context, index) {
+            final category = categories[index];
+
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ImageWidget(
+                      link: category.iconPath,
+                      width: 0.1.sw,
+                      height: 0.1.sw,
+                      fit: BoxFit.cover,
+                    ),
+                    SpacingFoundation.horizontalSpace4,
+                    Expanded(
+                      child: GradientableWidget(
+                        gradient: GradientFoundation.defaultLinearGradient,
+                        child: Text(
+                          category.title,
+                          style: boldTextTheme?.subHeadline.copyWith(color: Colors.white),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SpacingFoundation.verticalSpace16,
+                ...category.items.map(
+                  (content) {
+                    final isLast = category.items.indexOf(content) == category.items.length - 1;
+
+                    return Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadiusFoundation.all12,
+                          child: ImageWidget(
+                            link: content.imageUrl,
+                            width: contentImageWidth,
+                            height: contentImageHeight,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        SpacingFoundation.horizontalSpace12,
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Text(
+                                content.title,
+                                style: boldTextTheme?.caption1Bold,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              Text(
+                                formatDifference(content.datePosted),
+                                style: boldTextTheme?.caption1Medium.copyWith(color: ColorsFoundation.mutedText),
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              SpacingFoundation.verticalSpace8,
+                              UiKitTagsWidget(baseTags: content.tags),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ).paddingOnly(bottom: isLast ? 0 : EdgeInsetsFoundation.vertical16);
+                  },
+                ),
+              ],
+            );
+          },
+          itemCount: categories.length,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/ui_kit/organisms/profile/influencer_public_profile_card_body.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+
+class InfluencerPublicProfileCardBody extends StatelessWidget {
+  final String? name;
+  final String? nickname;
+  final String? description;
+  final String? avatarUrl;
+  final String? speciality;
+  final List<Widget>? socialLinks;
+  final List<String>? interests;
+  final List<String>? matchingInterests;
+  final VoidCallback? onShare;
+  final bool? hasAchievements;
+  final int followers;
+  final VoidCallback? onFollow;
+  final bool? following;
+
+  const InfluencerPublicProfileCardBody({
+    Key? key,
+    this.name,
+    this.nickname,
+    this.description,
+    this.avatarUrl,
+    this.speciality,
+    this.socialLinks,
+    this.interests,
+    this.matchingInterests,
+    this.onShare,
+    this.hasAchievements,
+    this.followers = 0,
+    this.onFollow,
+    this.following,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.uiKitTheme;
+    final boldTextTheme = theme?.boldTextTheme;
+    final regularTextTheme = theme?.regularTextTheme;
+    final colorScheme = theme?.colorScheme;
+
+    return UiKitCardWrapper(
+      color: colorScheme?.surface1,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              context.userAvatar(
+                size: UserAvatarSize.x60x60,
+                type: UserTileType.influencer,
+                userName: name ?? '',
+                imageUrl: avatarUrl,
+                showAchievements: hasAchievements ?? false,
+              ),
+              SpacingFoundation.horizontalSpace4,
+              Expanded(
+                child: PersonalProfileInfo(
+                  name: name,
+                  nickname: nickname ?? '',
+                  nameStyle: boldTextTheme?.caption1Bold,
+                  nicknameStyle: boldTextTheme?.caption1Medium.copyWith(color: colorScheme?.darkNeutral500),
+                ),
+              ),
+              SpacingFoundation.horizontalSpace4,
+              if (onShare != null)
+                GestureDetector(
+                  onTap: onShare,
+                  child: Icon(
+                    ShuffleUiKitIcons.share,
+                    color: theme?.colorScheme.darkNeutral800,
+                  ),
+                ),
+            ],
+          ),
+          SpacingFoundation.verticalSpace16,
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: GradientableWidget(
+                  gradient: GradientFoundation.defaultLinearGradient,
+                  child: Text(
+                    speciality ?? '',
+                    style: theme?.boldTextTheme.caption2Medium.copyWith(color: Colors.white),
+                    maxLines: 2,
+                  ),
+                ),
+              ),
+              if (socialLinks != null)
+                ...socialLinks!.map((e) {
+                  final index = socialLinks!.indexOf(e);
+
+                  return e.paddingOnly(
+                    right: index != socialLinks!.length - 1 ? EdgeInsetsFoundation.horizontal8 : EdgeInsetsFoundation.zero,
+                  );
+                }).toList(),
+            ],
+          ),
+          SpacingFoundation.verticalSpace16,
+          context.button(
+            data: BaseUiKitButtonData(
+              text: following == true ? S.current.Unfollow : S.current.Follow,
+              onPressed: onFollow,
+            ),
+          ),
+          SpacingFoundation.verticalSpace16,
+          RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text:
+                      '${NumberFormat('###,###', Localizations.localeOf(context).languageCode).format(followers).replaceAll(',', ' ')} ',
+                  style: boldTextTheme?.caption1Bold.copyWith(color: colorScheme?.bodyTypography),
+                ),
+                TextSpan(
+                  text: S.current.FollowersCount(followers),
+                  style: boldTextTheme?.caption1Medium.copyWith(color: ColorsFoundation.mutedText),
+                ),
+              ],
+            ),
+          ),
+          SpacingFoundation.verticalSpace16,
+          if (interests != null && matchingInterests != null)
+            ProfileInterests(
+              profileInterests: interests!,
+              matchingInterests: matchingInterests!,
+            ),
+          SpacingFoundation.verticalSpace16,
+          if (description != null)
+            Text(
+              description!,
+              style: boldTextTheme?.caption1Bold,
+            ),
+        ],
+      ).paddingAll(EdgeInsetsFoundation.all16),
+    );
+  }
+}
