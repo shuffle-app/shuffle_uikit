b291102ca55047419320c67e79d60983cc96109f
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-06-17 20:28:28 +0300

    fix

--- lib/ui_kit/atoms/inputs/ui_kit_emoji_inputs.dart
+++ lib/ui_kit/atoms/inputs/ui_kit_emoji_inputs.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/foundation.dart' as foundation;
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
 
 class UiKitEmojiInputs extends StatefulWidget {
@@ -19,6 +22,25 @@ class UiKitEmojiInputs extends StatefulWidget {
 
 class _UiKitEmojiInputsState extends State<UiKitEmojiInputs> {
   bool _showEmojiPicker = false;
+  bool _showKeyboard = false;
+  late final StreamSubscription<bool> keyboardSubscription;
+  final KeyboardVisibilityController keyboardVisibilityController =
+      KeyboardVisibilityController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    keyboardSubscription = keyboardVisibilityController.onChange.listen((bool visible) {
+      if (visible && _showKeyboard) _showEmojiPicker = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    keyboardSubscription.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,21 +49,50 @@ class _UiKitEmojiInputsState extends State<UiKitEmojiInputs> {
     return Column(
       children: [
         SpacingFoundation.verticalSpace12,
-        UiKitInputFieldRightIcon(
-          controller: widget.textEditingController,
-          icon: context.iconButtonNoPadding(
-            data: BaseUiKitButtonData(
-              iconInfo: BaseUiKitButtonIconData(
-                iconData: ShuffleUiKitIcons.moodhappy,
+        Row(
+          children: [
+            Expanded(
+              child: TapRegion(
+                onTapOutside: (tap) {
+                  setState(() {
+                    _showKeyboard = false;
+                  });
+                },
+                child: UiKitInputFieldRightIcon(
+                  onTap: () {
+                    setState(() {
+                      _showKeyboard = true;
+                      _showEmojiPicker = false;
+                    });
+                  },
+                  controller: widget.textEditingController,
+                  icon: context.iconButtonNoPadding(
+                    data: BaseUiKitButtonData(
+                      iconInfo: BaseUiKitButtonIconData(
+                        iconData: ShuffleUiKitIcons.moodhappy,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _showEmojiPicker = !_showEmojiPicker;
+                          FocusScope.of(context).requestFocus(FocusNode());
+                        });
+                      },
+                    ),
+                  ),
+                ),
               ),
-              onPressed: () {
-                setState(() {
-                  _showEmojiPicker = !_showEmojiPicker;
-                  FocusScope.of(context).requestFocus(FocusNode());
-                });
-              },
             ),
-          ),
+            SpacingFoundation.horizontalSpace8,
+            context.iconButtonNoPadding(
+              data: BaseUiKitButtonData(
+                iconInfo: BaseUiKitButtonIconData(
+                  iconData: ShuffleUiKitIcons.send,
+                  size: 0.07.sw,
+                ),
+                onPressed: widget.onSend,
+              ),
+            ),
+          ],
         ),
         SpacingFoundation.verticalSpace12,
         if (_showEmojiPicker) ...[
@@ -58,9 +109,7 @@ class _UiKitEmojiInputsState extends State<UiKitEmojiInputs> {
                     (foundation.defaultTargetPlatform == TargetPlatform.iOS ? 1.20 : 1.0),
               ),
               swapCategoryAndBottomBar: false,
-              skinToneConfig: const SkinToneConfig(
-                dialogBackgroundColor: Colors.red,
-              ),
+              skinToneConfig: const SkinToneConfig(),
               categoryViewConfig: CategoryViewConfig(
                 backgroundColor: theme?.colorScheme.surface1 ?? Colors.white,
               ),
@@ -73,6 +122,8 @@ class _UiKitEmojiInputsState extends State<UiKitEmojiInputs> {
                 backgroundColor: theme?.colorScheme.surface1 ?? Colors.white,
                 buttonIconColor: theme?.colorScheme.inversePrimary ?? Colors.white,
                 hintText: '',
+                textStyle: theme?.regularTextTheme.caption1 ??
+                    TextStyle(color: theme?.colorScheme.inversePrimary),
               ),
             ),
           ),
