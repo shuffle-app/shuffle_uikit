3ee8bc7edd79658aad73ce66e80c8067af77565b
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-06-20 13:23:23 +0300

    feat: add videoFile

--- lib/ui_kit/molecules/moderation/ui_kit_video_reaction_moderation.dart
+++ lib/ui_kit/molecules/moderation/ui_kit_video_reaction_moderation.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
 
 class UiKitVideoReactionsModeration extends StatelessWidget {
+  final dynamic videoFile;
   final UiKitFeedbackInfo feedbackInfo;
 
   const UiKitVideoReactionsModeration({
     super.key,
     required this.feedbackInfo,
+    required this.videoFile,
   });
 
   @override
@@ -15,7 +17,7 @@ class UiKitVideoReactionsModeration extends StatelessWidget {
       children: [
         Expanded(
           child: VideoCutter(
-            videoFile: null,
+            videoFile: videoFile,
             onExportFinished: (exportPath) {},
             outputDirectory: 'output',
           ),
