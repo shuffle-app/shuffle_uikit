a6ed0fc86d30e6aaa51eb0b2d6b8b410f7eab710
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-06-24 21:42:11 +0300

    feat: add custom title for UiKitHintDialog

--- lib/ui_kit/templates/ui_kit_hint_dialog.dart
+++ lib/ui_kit/templates/ui_kit_hint_dialog.dart
@@ -8,6 +8,7 @@ class UiKitHintDialog extends StatelessWidget {
   final String subtitle;
   final String dismissText;
   final VoidCallback onDismiss;
+  final Widget? customTitle;
 
   const UiKitHintDialog({
     Key? key,
@@ -17,6 +18,7 @@ class UiKitHintDialog extends StatelessWidget {
     required this.subtitle,
     required this.dismissText,
     required this.onDismiss,
+    this.customTitle,
   }) : super(key: key);
 
   @override
@@ -25,11 +27,13 @@ class UiKitHintDialog extends StatelessWidget {
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Text(
-          title,
-          style: textStyle,
-          textAlign: TextAlign.center,
-        ),
+        customTitle != null
+            ? customTitle!
+            : Text(
+                title,
+                style: textStyle,
+                textAlign: TextAlign.center,
+              ),
         SpacingFoundation.verticalSpace8,
         GridView.count(
           physics: const NeverScrollableScrollPhysics(),
