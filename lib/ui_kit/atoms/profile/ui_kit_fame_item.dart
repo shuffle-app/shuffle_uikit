fb9db1a4795aabdddb92c1654c54a0db4bf1ff6b
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-07-10 20:15:18 +0300

    fear: test

--- lib/ui_kit/atoms/profile/ui_kit_fame_item.dart
+++ lib/ui_kit/atoms/profile/ui_kit_fame_item.dart
@@ -190,13 +190,219 @@ _showModelViewerDialog(BuildContext context, String filePath, String filePoster)
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadiusFoundation.all24,
         ),
-        child: SizedBox(
-            height: 0.4.sh,
-            child: UiKitBase3DViewer(
-              localPath: filePath,
-              poster: filePoster,
-              autoRotate: true,
-              // environmentImage: 'https://shuffle-app-production.s3.eu-west-2.amazonaws.com/static-files/3dmodels/environments/environment1.jpeg',
-            )),
+        // child: SizedBox(
+        //     height: 0.4.sh,
+        //     child: UiKitBase3DViewer(
+        //       localPath: filePath,
+        //       poster: filePoster,
+        //       autoRotate: true,
+        //       // environmentImage: 'https://shuffle-app-production.s3.eu-west-2.amazonaws.com/static-files/3dmodels/environments/environment1.jpeg',
+        //     )),
+        child: PrizeDialog(
+          filePath: filePath,
+          filePoster: filePoster,
+          uiRewardProgressModel: UiRewardProgressModel(
+            current: 50,
+            total: 100,
+            title: 'Test',
+          ),
+        ),
       );
     });
+
+class PrizeDialog extends StatelessWidget {
+  final UiRewardProgressModel? uiRewardProgressModel;
+  final String filePath;
+  final String filePoster;
+  const PrizeDialog({
+    super.key,
+    this.uiRewardProgressModel,
+    required this.filePath,
+    required this.filePoster,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.uiKitTheme;
+
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        GestureDetector(
+          onTap: () => Navigator.of(context).pop(),
+          child: ImageWidget(
+            iconData: ShuffleUiKitIcons.cross,
+            color: theme?.colorScheme.darkNeutral900,
+            height: 19.h,
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        SpacingFoundation.verticalSpace16,
+        UiKitCardWrapper(
+          color: theme?.colorScheme.surface,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                uiRewardProgressModel?.title ?? S.of(context).NothingFound,
+                style: theme?.boldTextTheme.title2,
+              ),
+              SizedBox(height: 35.h),
+              Center(
+                child: Stack(children: [
+                  Transform.rotate(
+                    angle: 45 * math.pi / 180,
+                    child: SizedBox(
+                      height: 130.h,
+                      width: 190.w,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadiusFoundation.all24r,
+                          color: ColorsFoundation.darkNeutral100.withOpacity(0.16),
+                          backgroundBlendMode: BlendMode.plus,
+                          gradient: GradientFoundation.fameLinearGradient,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 130.h,
+                    width: 190.w,
+                    child: UiKitBase3DViewer(
+                      localPath: filePath,
+                      poster: filePoster,
+                      autoRotate: true,
+                      // environmentImage:
+                      //     'https://shuffle-app-production.s3.eu-west-2.amazonaws.com/static-files/3dmodels/environments/environment1.jpeg',
+                    ),
+                  ),
+                ]),
+              ),
+              SizedBox(height: 50.h),
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Points and Levels: ',
+                      style: theme?.boldTextTheme.caption2Bold,
+                    ),
+                    TextSpan(
+                      text: 'Users can earn points or level up based on their achievements.\n',
+                      style: theme?.regularTextTheme.caption2,
+                    ),
+                    TextSpan(
+                      text: 'Unlockable Rewards: ',
+                      style: theme?.boldTextTheme.caption2Bold,
+                    ),
+                    TextSpan(
+                      text: 'Special features, charms, or badges.',
+                      style: theme?.regularTextTheme.caption2,
+                    ),
+                  ],
+                ),
+              ),
+              SpacingFoundation.verticalSpace16,
+              Text(
+                '100 Reviews',
+                style: theme?.boldTextTheme.body,
+              ),
+              SpacingFoundation.verticalSpace4,
+              Row(
+                children: [
+                  Text(
+                    S.of(context).YourProgress,
+                    style: theme?.regularTextTheme.caption1,
+                  ),
+                  const Spacer(),
+                  Text(
+                    '${uiRewardProgressModel?.current.toInt() ?? 0}/${uiRewardProgressModel?.total.toInt() ?? 1}',
+                    style: theme?.regularTextTheme.caption1,
+                  ),
+                ],
+              ),
+              SpacingFoundation.verticalSpace2,
+              LinearInfluencerIndicator(
+                width: 0.81.sw,
+                actualSum: uiRewardProgressModel?.current ?? 0.0,
+                sum: uiRewardProgressModel?.total ?? 1.0,
+              )
+            ],
+          ).paddingAll(EdgeInsetsFoundation.all24),
+        )
+      ],
+    ).paddingSymmetric(horizontal: SpacingFoundation.horizontalSpacing16);
+  }
+}
+
+class UiRewardProgressModel {
+  final String? title;
+  final double current;
+  final double total;
+
+  UiRewardProgressModel({
+    this.title,
+    this.current = 1,
+    this.total = 1,
+  });
+}
+
+class LinearInfluencerIndicator extends StatelessWidget {
+  final double actualSum;
+  final double sum;
+  final double? width;
+
+  LinearInfluencerIndicator({
+    super.key,
+    required this.actualSum,
+    required this.sum,
+    this.width,
+  });
+
+  late final double _indicatorWidth = width ?? 256.w;
+
+  double get _progressPosition => _indicatorWidth * (_progressValue / 120);
+  double get _progressValue => ((actualSum / sum) * 120);
+
+  double _getCurrentPosition(double currentPosition) {
+    if (currentPosition > _indicatorWidth) {
+      return _indicatorWidth;
+    }
+
+    return currentPosition;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = context.uiKitTheme?.colorScheme;
+
+    return ClipRRect(
+      borderRadius: BorderRadiusFoundation.all40,
+      child: Stack(
+        alignment: Alignment.centerLeft,
+        children: [
+          ColoredBox(
+            color: colorScheme?.surface3 ?? Colors.white,
+            child: SizedBox(
+              height: 6.h,
+              width: _indicatorWidth,
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: AnimatedContainer(
+                  width: _getCurrentPosition(_progressPosition),
+                  curve: Curves.ease,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadiusFoundation.all40,
+                    gradient: GradientFoundation.touchIdLinearGradient,
+                    color: Colors.white,
+                  ),
+                  duration: const Duration(milliseconds: 300),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
