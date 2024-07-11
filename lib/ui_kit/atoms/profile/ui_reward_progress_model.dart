d2ec57d66cfd6aa9e1f9b20b9eb97b2c719c7fbb
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-07-11 12:28:46 +0300

    feat: add UiRewardProgressModel

---
+++ lib/ui_kit/atoms/profile/ui_reward_progress_model.dart
@@ -0,0 +1,11 @@
+class UiRewardProgressModel {
+  final String? title;
+  final double current;
+  final double total;
+
+  UiRewardProgressModel({
+    this.title,
+    this.current = 1,
+    this.total = 1,
+  });
+}
