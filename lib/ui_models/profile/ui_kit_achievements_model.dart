5ea158c6ca8305dcd39663c47188bdd2c0d41070
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-07-11 15:49:11 +0300

    feat: add  current and total for UiKitAchievementsModel

--- lib/ui_models/profile/ui_kit_achievements_model.dart
+++ lib/ui_models/profile/ui_kit_achievements_model.dart
@@ -1,18 +1,35 @@
-
 class UiKitAchievementsModel {
   final String title;
   final String? asset;
   final String? objectUrl;
   final String? animationName;
+  final double? current;
+  final double? total;
 
-  const UiKitAchievementsModel({required this.title, this.asset, this.objectUrl, this.animationName});
+  const UiKitAchievementsModel({
+    required this.title,
+    this.asset,
+    this.objectUrl,
+    this.animationName,
+    this.current,
+    this.total,
+  });
 
-  UiKitAchievementsModel copyWith({String? title, String? asset, String? objectUrl, String? animationName}) =>
+  UiKitAchievementsModel copyWith({
+    String? title,
+    String? asset,
+    String? objectUrl,
+    String? animationName,
+    double? current,
+    double? total,
+  }) =>
       UiKitAchievementsModel(
         title: title ?? this.title,
         asset: asset ?? this.asset,
         objectUrl: objectUrl ?? this.objectUrl,
         animationName: animationName ?? this.animationName,
+        current: current ?? this.current,
+        total: total ?? this.total,
       );
 
   @override
