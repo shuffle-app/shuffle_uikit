51d9a0d3ebf6390540bcbcab9667fa7be8f6810c
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-07-11 16:41:52 +0300

    fix

--- example/lib/presentation/ui/examples/hall_of_fame_page_example.dart
+++ example/lib/presentation/ui/examples/hall_of_fame_page_example.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
 import 'package:shuffle_uikit/ui_kit/atoms/profile/ui_reward_progress_model.dart';
-import 'package:shuffle_uikit/ui_kit/atoms/profile/ui_kit_fame_item_dialog.dart';
 
 class HallOfFamePageExample extends StatelessWidget {
   const HallOfFamePageExample({super.key});
@@ -13,27 +12,9 @@ class HallOfFamePageExample extends StatelessWidget {
       (index) => GridTitledItemWidget(
         title: 'Title $index',
         child: UiKitFameItem(
-          // showModelViewerDialog: (context, filePath, filePoster, uiRewardProgressModel) => showModelViewerDialog(
-          //   context,
-          //   filePath,
-          //   filePoster,
-          //   uiRewardProgressModel,
-          // ),
-          // uiRewardProgressModel: UiRewardProgressModel(
-          //   current: 90,
-          //   total: 100,
-          //   title: 'TEst',
-          // ),
-          // uiModel: UiKitAchievementsModel(
-          //   asset: index % 5 == 0 ? null : GraphicsFoundation.instance.png.goldenCup.path,
-          //   title: 'Title $index',
-          // ),
           uiModel: UiKitAchievementsModel(
-            title: 'Ring',
-            objectUrl:
-                'https://shuffle-app-production.s3.eu-west-2.amazonaws.com/static-files/3dmodels/objects/luxury_ring_with_ru.glb',
-            asset:
-                'https://shuffle-app-production.s3.eu-west-2.amazonaws.com/static-files/3dmodels/posters/ring_poster.webp',
+            asset: index % 5 == 0 ? null : GraphicsFoundation.instance.png.goldenCup.path,
+            title: 'Title $index',
           ),
         ),
       ),
