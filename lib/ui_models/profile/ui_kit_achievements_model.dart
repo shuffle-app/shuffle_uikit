4bd014062846758d8e26052198612c0222ae39bf
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-07-11 17:37:30 +0300

    fix: remove total and current, add UiRewardProgressModel, rename asset to posterUrl

--- lib/ui_models/profile/ui_kit_achievements_model.dart
+++ lib/ui_models/profile/ui_kit_achievements_model.dart
@@ -1,23 +1,23 @@
+import 'package:shuffle_uikit/ui_kit/atoms/profile/ui_reward_progress_model.dart';
+
 class UiKitAchievementsModel {
   final String title;
-  final String? asset;
+  final String? posterUrl;
   final String? objectUrl;
   final String? animationName;
-  final double? current;
-  final double? total;
+  final UiRewardProgressModel? uiRewardProgressModel;
 
   const UiKitAchievementsModel({
     required this.title,
-    this.asset,
+    this.posterUrl,
     this.objectUrl,
     this.animationName,
-    this.current,
-    this.total,
+    this.uiRewardProgressModel,
   });
 
   UiKitAchievementsModel copyWith({
     String? title,
-    String? asset,
+    String? posterUrl,
     String? objectUrl,
     String? animationName,
     double? current,
@@ -25,18 +25,16 @@ class UiKitAchievementsModel {
   }) =>
       UiKitAchievementsModel(
         title: title ?? this.title,
-        asset: asset ?? this.asset,
+        posterUrl: posterUrl ?? this.posterUrl,
         objectUrl: objectUrl ?? this.objectUrl,
         animationName: animationName ?? this.animationName,
-        current: current ?? this.current,
-        total: total ?? this.total,
       );
 
   @override
   bool operator ==(Object other) {
-    return other is UiKitAchievementsModel && (asset == other.asset || objectUrl == other.objectUrl);
+    return other is UiKitAchievementsModel && (posterUrl == other.posterUrl || objectUrl == other.objectUrl);
   }
 
   @override
-  int get hashCode => asset?.hashCode ?? objectUrl?.hashCode ?? title.hashCode;
+  int get hashCode => posterUrl?.hashCode ?? objectUrl?.hashCode ?? title.hashCode;
 }
