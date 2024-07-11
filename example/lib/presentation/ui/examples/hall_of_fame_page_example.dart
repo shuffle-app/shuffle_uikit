d4752800507cce938b5509346586872ff91b9ade
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-07-11 12:28:30 +0300

    feat: add example

--- example/lib/presentation/ui/examples/hall_of_fame_page_example.dart
+++ example/lib/presentation/ui/examples/hall_of_fame_page_example.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
+import 'package:shuffle_uikit/ui_kit/atoms/profile/ui_reward_progress_model.dart';
 
 class HallOfFamePageExample extends StatelessWidget {
   const HallOfFamePageExample({super.key});
@@ -7,15 +8,29 @@ class HallOfFamePageExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final listOfItems = List.generate(
-        20,
-        (index) => GridTitledItemWidget(
-              title: 'Title $index',
-              child: UiKitFameItem(
-                  uiModel: UiKitAchievementsModel(
-                asset: index % 5 == 0 ? null : GraphicsFoundation.instance.png.goldenCup.path,
-                title: 'Title $index',
-              )),
-            ));
+      20,
+      (index) => GridTitledItemWidget(
+        title: 'Title $index',
+        child: UiKitFameItem(
+          uiRewardProgressModel: UiRewardProgressModel(
+            current: 90,
+            total: 100,
+            title: 'TEst',
+          ),
+          // uiModel: UiKitAchievementsModel(
+          //   asset: index % 5 == 0 ? null : GraphicsFoundation.instance.png.goldenCup.path,
+          //   title: 'Title $index',
+          // ),
+          uiModel: UiKitAchievementsModel(
+            title: 'Ring',
+            objectUrl:
+                'https://shuffle-app-production.s3.eu-west-2.amazonaws.com/static-files/3dmodels/objects/luxury_ring_with_ru.glb',
+            asset:
+                'https://shuffle-app-production.s3.eu-west-2.amazonaws.com/static-files/3dmodels/posters/ring_poster.webp',
+          ),
+        ),
+      ),
+    );
 
     return Scaffold(
       body: BlurredAppBarPage(
