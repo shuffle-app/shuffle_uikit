a8d9fac1f04cfffb3ecc9cb820de7e1cab349346
romancores <225000@niuitmo.ru>
2024-08-22 10:41:37 +0300

    small fixes for properties panel

--- lib/ui_kit/molecules/tiles/ui_kit_hover_animation_tile.dart
+++ lib/ui_kit/molecules/tiles/ui_kit_hover_animation_tile.dart
@@ -12,8 +12,7 @@ class UiKitHoverAnimationTile extends StatefulWidget {
   final ValueChanged<String> onTap;
 
   @override
-  State<UiKitHoverAnimationTile> createState() =>
-      _UiKitHoverAnimationTileState();
+  State<UiKitHoverAnimationTile> createState() => _UiKitHoverAnimationTileState();
 }
 
 class _UiKitHoverAnimationTileState extends State<UiKitHoverAnimationTile> {
@@ -28,53 +27,43 @@ class _UiKitHoverAnimationTileState extends State<UiKitHoverAnimationTile> {
   Widget build(BuildContext context) {
     final uiKitTheme = context.uiKitTheme;
 
-    return Material(
-      color: Colors.transparent,
-      child: InkWell(
-        onTap: () => widget.onTap.call(widget.title),
-        onHover: (value) {
-          setState(() {
-            isHover = value;
-          });
-        },
-        child: Ink(
-          decoration: BoxDecoration(
-            color: isHover
-                ? ColorsFoundation.onHover.withOpacity(0.08)
-                : uiKitTheme?.colorScheme.surface1,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                widget.title,
-                style: uiKitTheme?.regularTextTheme.body
-                    .copyWith(color: uiKitTheme.colorScheme.bodyTypography),
-              ),
-              if (isHover)
-                context.boxIconButton(
-                  data: BaseUiKitButtonData(
-                    onPressed: () {
-                      widget.onTap.call(widget.title);
-                    },
-                    backgroundColor: ColorsFoundation.success.withOpacity(0.3),
-                    iconInfo: BaseUiKitButtonIconData(
-                        iconData: ShuffleUiKitIcons.plus,
-                        color: ColorsFoundation.onHover,
-                        size: 16.sp),
-                  ),
-                )
-              else
-                Icon(
-                  ShuffleUiKitIcons.plus,
-                  size: 16.sp,
-                  color: Colors.transparent,
-                ).paddingAll(EdgeInsetsFoundation.all6),
-            ],
-          ).paddingSymmetric(
-              horizontal: EdgeInsetsFoundation.horizontal16,
-              vertical: EdgeInsetsFoundation.vertical12),
+    return InkWell(
+      onTap: () => widget.onTap.call(widget.title),
+      onHover: (value) {
+        setState(() {
+          isHover = value;
+        });
+      },
+      child: Ink(
+        decoration: BoxDecoration(
+          color: isHover ? ColorsFoundation.onHover.withOpacity(0.08) : uiKitTheme?.colorScheme.surface1,
         ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              widget.title,
+              style: uiKitTheme?.regularTextTheme.body.copyWith(color: uiKitTheme.colorScheme.bodyTypography),
+            ),
+            if (isHover)
+              context.boxIconButton(
+                data: BaseUiKitButtonData(
+                  onPressed: () {
+                    widget.onTap.call(widget.title);
+                  },
+                  backgroundColor: ColorsFoundation.success.withOpacity(0.3),
+                  iconInfo: BaseUiKitButtonIconData(
+                      iconData: ShuffleUiKitIcons.plus, color: ColorsFoundation.onHover, size: 16.sp),
+                ),
+              )
+            else
+              Icon(
+                ShuffleUiKitIcons.plus,
+                size: 16.sp,
+                color: Colors.transparent,
+              ).paddingAll(EdgeInsetsFoundation.all6),
+          ],
+        ).paddingSymmetric(horizontal: EdgeInsetsFoundation.horizontal16, vertical: EdgeInsetsFoundation.vertical12),
       ),
     );
   }
