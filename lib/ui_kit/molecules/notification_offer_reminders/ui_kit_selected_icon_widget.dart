a2f6a869e68ffab5fd423962aebd1fec8650a30d
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-09-06 17:02:52 +0300

    feat: add UiKitSelectedIconWidget

---
+++ lib/ui_kit/molecules/notification_offer_reminders/ui_kit_selected_icon_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+
+class UiKitSelectedIconWidget extends StatelessWidget {
+  final List<String>? iconList;
+  final ValueChanged<int>? onIconTap;
+  final int? selectedIndex;
+
+  const UiKitSelectedIconWidget({
+    super.key,
+    this.iconList,
+    this.onIconTap,
+    this.selectedIndex,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 45.h,
+      child: ListView.separated(
+        itemCount: iconList?.length ?? 0,
+        separatorBuilder: (context, index) => SpacingFoundation.horizontalSpace16,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () => onIconTap?.call(index),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                border: selectedIndex == index ? GradientFoundation.gradientBorder : null,
+                borderRadius: BorderRadiusFoundation.all24r,
+              ),
+              child: ImageWidget(
+                height: 45.h,
+                fit: BoxFit.fitHeight,
+                link: iconList?[index],
+              ).paddingAll(EdgeInsetsFoundation.all2),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
