f9db6731d6cba1ad93892720cdf2a595cf30928a
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-10-07 17:11:46 +0300

    feat: add change for offer item in SpentPointsComponent

--- lib/ui_kit/organisms/cards/ui_kit_extended_influencer_feedback_card_without_bottom.dart
+++ lib/ui_kit/organisms/cards/ui_kit_extended_influencer_feedback_card_without_bottom.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
+//TODO
 /// думаю стоит перенепсти этот виджет в компонентную библиотеку разобрав его на атомы
 class UiKitExtendedInfluencerFeedbackCardWithoutBottom extends StatelessWidget {
   final String? title;
   final String? imageUrl;
   final List<UiKitTag>? tags;
+  final DateTime? periodFrom;
+  final DateTime? periodTo;
+  final String? contentTitle;
 
   const UiKitExtendedInfluencerFeedbackCardWithoutBottom({
     super.key,
     this.title,
     this.imageUrl,
     this.tags,
+    this.periodFrom,
+    this.periodTo,
+    this.contentTitle,
   });
 
   @override
@@ -20,11 +28,11 @@ class UiKitExtendedInfluencerFeedbackCardWithoutBottom extends StatelessWidget {
       builder: (context, size) {
         final theme = context.uiKitTheme;
         final textTheme = theme?.boldTextTheme;
-        final titleStyle = textTheme?.caption2Bold
-            .copyWith(
-            color: theme?.themeMode == ThemeMode.dark ? ColorsFoundation
-                .lightBodyTypographyColor :ColorsFoundation.
-            darkBodyTypographyColor);
+        // final titleStyle = textTheme?.caption2Bold.copyWith(
+        //     color: theme?.themeMode == ThemeMode.dark
+        //         ? ColorsFoundation.lightBodyTypographyColor
+        //         : ColorsFoundation.darkBodyTypographyColor);
+        final titleStyle = textTheme?.caption2Bold;
         final width = size.maxWidth;
 
         return Column(
@@ -48,33 +56,57 @@ class UiKitExtendedInfluencerFeedbackCardWithoutBottom extends StatelessWidget {
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      title ?? '',
-                      style: titleStyle,
-                    ),
-                    SpacingFoundation.verticalSpace4,
                     SizedBox(
-                      width: (size.maxWidth * 0.7) -
-                          SpacingFoundation.horizontalSpacing10,
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            ...tags
-                                ?.map<Widget>(
-                                  (e) =>
-                                  UiKitTagWidget(
-                                    title: e.title,
-                                    icon: e.icon,
-                                    showSpacing: tags?.indexOf(e) != 0,
-                                  ),
-                            )
-                                .toList() ??
-                                [],
-                          ],
-                        ),
+                      width: (size.maxWidth * 0.7) - SpacingFoundation.horizontalSpacing10,
+                      child: AutoSizeText(
+                        title ?? '',
+                        style: titleStyle,
                       ),
                     ),
+                    SpacingFoundation.verticalSpace2,
+                    if (contentTitle != null && contentTitle!.isNotEmpty)
+                      SizedBox(
+                        width: (size.maxWidth * 0.7) - SpacingFoundation.horizontalSpacing10,
+                        child: AutoSizeText(
+                          contentTitle!,
+                          style: textTheme?.caption2Bold.copyWith(color: ColorsFoundation.mutedText),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                      ),
+                    if (tags != null && tags!.isNotEmpty)
+                      SizedBox(
+                        width: (size.maxWidth * 0.7) - SpacingFoundation.horizontalSpacing10,
+                        child: SingleChildScrollView(
+                          scrollDirection: Axis.horizontal,
+                          child: Row(
+                            children: [
+                              ...tags
+                                      ?.map<Widget>(
+                                        (e) => UiKitTagWidget(
+                                          title: e.title,
+                                          icon: e.icon,
+                                          showSpacing: tags?.indexOf(e) != 0,
+                                        ),
+                                      )
+                                      .toList() ??
+                                  [],
+                            ],
+                          ),
+                        ),
+                      ),
+                    SpacingFoundation.verticalSpace2,
+                    if (periodFrom != null && periodTo != null)
+                      Text(
+                        '${formatDateWithCustomPattern(
+                          'dd.MM',
+                          (periodFrom ?? DateTime.now()).toLocal(),
+                        )} - ${formatDateWithCustomPattern(
+                          'dd.MM.yyyy',
+                          (periodTo ?? DateTime.now()).toLocal(),
+                        )}',
+                        style: theme?.regularTextTheme.caption4Regular.copyWith(color: ColorsFoundation.mutedText),
+                      ),
                   ],
                 ),
               ],
