6071e6d6f8761a4a6dbbdc6b1f32c661f71dcabf
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-10-17 16:09:11 +0300

    feat: add iconPath for NavBarTile

--- lib/ui_kit/molecules/nav_bar/nav_bar_tile.dart
+++ lib/ui_kit/molecules/nav_bar/nav_bar_tile.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
 
 class NavBarTile extends StatelessWidget {
-  final IconData icon;
+  final IconData? icon;
+  final String? iconPath;
   final bool selected;
 
   const NavBarTile({
     super.key,
-    required this.icon,
+    this.icon,
+    this.iconPath,
     required this.selected,
   });
 
@@ -21,6 +23,7 @@ class NavBarTile extends StatelessWidget {
       children: [
         ImageWidget(
           iconData: icon,
+          link: iconPath,
           color: color,
         ),
         SpacingFoundation.verticalSpace4,
