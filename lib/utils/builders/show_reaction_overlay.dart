be8089eca51e0820da03fe10d69868c81c4cb250
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-11-02 00:16:53 +0300

    feat: add showReactionOverlay and hideReactionOverlay

---
+++ lib/utils/builders/show_reaction_overlay.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import '../../shuffle_uikit.dart';
+
+void hideReactionOverlay(OverlayEntry? overlayEntry) {
+  overlayEntry?.remove();
+  overlayEntry = null;
+}
+
+void showReactionOverlay(
+  BuildContext context,
+  OverlayEntry? overlayEntry,
+  Color? reactionTextColor,
+  ValueChanged<String>? onReactionsTapped,
+) {
+  RenderBox renderBox = context.findRenderObject() as RenderBox;
+  Offset position = renderBox.localToGlobal(Offset.zero);
+  overlayEntry = OverlayEntry(
+    builder: (context) => Positioned(
+      top: position.dy - 40,
+      right: position.dx - 20,
+      child: Material(
+        color: Colors.transparent,
+        child: UiKitCardWrapper(
+          padding: EdgeInsets.all(EdgeInsetsFoundation.all8),
+          child: TapRegion(
+            onTapOutside: (event) {
+              hideReactionOverlay.call(overlayEntry);
+            },
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                UiKitHeartEyesReaction(
+                  reactionsCount: 0,
+                  textColor: reactionTextColor,
+                  onTap: () {
+                    onReactionsTapped?.call('heart_eyes');
+                    hideReactionOverlay(overlayEntry);
+                  },
+                ),
+                SpacingFoundation.horizontalSpace4,
+                UiKitLikeReaction(
+                  reactionsCount: 0,
+                  textColor: reactionTextColor,
+                  onTap: () {
+                    onReactionsTapped?.call('like');
+                    hideReactionOverlay(overlayEntry);
+                  },
+                ),
+                SpacingFoundation.horizontalSpace4,
+                UiKitFireReaction(
+                  reactionsCount: 0,
+                  textColor: reactionTextColor,
+                  onTap: () {
+                    onReactionsTapped?.call('fire');
+                    hideReactionOverlay(overlayEntry);
+                  },
+                ),
+                SpacingFoundation.horizontalSpace4,
+                UiKitSunglassesReaction(
+                  reactionsCount: 0,
+                  textColor: reactionTextColor,
+                  onTap: () {
+                    onReactionsTapped?.call('sunglasses');
+                    hideReactionOverlay(overlayEntry);
+                  },
+                ),
+                SpacingFoundation.horizontalSpace4,
+                UiKitSmileyReaction(
+                  reactionsCount: 0,
+                  onTap: () {
+                    onReactionsTapped?.call('smiley');
+                    hideReactionOverlay(overlayEntry);
+                  },
+                  textColor: reactionTextColor,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+
+  Overlay.of(context).insert(overlayEntry);
+}
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -40,3 +40,4 @@ export 'text_input_formater/only_numbers_formatter.dart';
 export 'text_input_formater/string_with_space.dart';
 export 'format_date_time/is_time_within_range.dart';
 export 'text_formatter/double_format.dart';
+export 'builders/show_reaction_overlay.dart';
