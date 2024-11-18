53f2645b021701dbc15e7590bcd9e7808b381680
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-11-18 20:19:17 +0300

    feat: add showCitiesOverlay for admin panel

---
+++ lib/utils/builders/show_city_choice.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+
+void showCitiesOverlay({
+  required BuildContext context,
+  OverlayEntry? overlayEntry,
+  ValueChanged<String>? onCityTapped,
+  List<String>? cities,
+  String? selectedCity,
+}) {
+  final theme = context.uiKitTheme;
+  RenderBox renderBox = context.findRenderObject() as RenderBox;
+  Offset position = renderBox.localToGlobal(Offset.zero);
+
+  overlayEntry = OverlayEntry(
+    builder: (context) => Positioned(
+      top: (position.dy / 2) + 10.h,
+      right: 10.w,
+      child: Material(
+        color: Colors.transparent,
+        child: Container(
+          decoration: BoxDecoration(
+            color: theme?.colorScheme.surface5,
+            borderRadius: BorderRadiusFoundation.all16,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.white.withOpacity(0.3),
+                blurRadius: 5.0,
+                spreadRadius: 5.0,
+              ),
+            ],
+          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: SpacingFoundation.horizontalSpacing24,
+            vertical: SpacingFoundation.verticalSpacing12,
+          ),
+          child: TapRegion(
+            onTapOutside: (event) {
+              hideCitiesOverlay(overlayEntry);
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (cities != null && cities.isNotEmpty)
+                  ...cities.map(
+                    (city) {
+                      return InkWell(
+                        onTap: () {
+                          onCityTapped?.call(city);
+                          hideCitiesOverlay(overlayEntry);
+                        },
+                        child: Row(
+                          children: [
+                            Visibility(
+                              visible: selectedCity == city,
+                              maintainSize: true,
+                              maintainAnimation: true,
+                              maintainState: true,
+                              child: const UiKitRadio(selected: true)
+                                  .paddingOnly(right: SpacingFoundation.horizontalSpacing12),
+                            ),
+                            Text(
+                              city,
+                              style: theme?.boldTextTheme.labelLarge,
+                            ),
+                          ],
+                        ).paddingSymmetric(vertical: SpacingFoundation.verticalSpacing12),
+                      );
+                    },
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+  Overlay.of(context).insert(overlayEntry);
+}
+
+void hideCitiesOverlay(OverlayEntry? overlayEntry) {
+  overlayEntry?.remove();
+  overlayEntry = null;
+}
