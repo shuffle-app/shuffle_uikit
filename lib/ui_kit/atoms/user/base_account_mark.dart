8740fe7d3f27f5840fc74d80fb249a535d5e6890
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-11-29 15:07:46 +0300

    fix: size of BaseAccountMark

--- lib/ui_kit/atoms/user/base_account_mark.dart
+++ lib/ui_kit/atoms/user/base_account_mark.dart
@@ -4,15 +4,22 @@ import 'package:shuffle_uikit/shuffle_uikit.dart';
 class BaseAccountMark extends StatelessWidget {
   final String iconLink;
   final Color? color;
+  final double? width;
 
   const BaseAccountMark({
     super.key,
     required this.iconLink,
     this.color,
+    this.width,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ImageWidget(link: iconLink, color: color);
+    return ImageWidget(
+      link: iconLink,
+      color: color,
+      width: width,
+      fit: BoxFit.fill,
+    );
   }
 }
