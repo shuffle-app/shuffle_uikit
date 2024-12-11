0e3cc4cdfb6cb23ea7c8e06b34208992b8007107
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-12-11 12:42:54 +0300

    add gray color for barcode if he was activated

--- lib/ui_kit/organisms/barcode/custom_barcode.dart
+++ lib/ui_kit/organisms/barcode/custom_barcode.dart
@@ -7,6 +7,7 @@ class CustomBarcode extends StatelessWidget {
   final double? width;
   final double? height;
   final Color? color;
+  final bool isBeenActivated;
 
   const CustomBarcode({
     super.key,
@@ -14,28 +15,42 @@ class CustomBarcode extends StatelessWidget {
     this.width,
     this.height,
     this.color,
+    this.isBeenActivated = false,
   });
 
   @override
   Widget build(BuildContext context) {
     final theme = context.uiKitTheme;
 
-    return UiKitCardWrapper(
-      width: width ?? 0.85.sw,
-      height: height ?? 0.24.sh,
-      color: color ?? theme?.colorScheme.inversePrimary,
-      borderRadius: BorderRadiusFoundation.all24r,
-      child: BarcodeWidget(
-        barcode: Barcode.code128(escapes: true),
-        data: barcodeNumber,
-        style: theme?.regularTextTheme.caption2.copyWith(color: theme.colorScheme.primary),
-        textPadding: SpacingFoundation.verticalSpacing8,
-      ).paddingOnly(
-        top: SpacingFoundation.verticalSpacing10,
-        bottom: SpacingFoundation.verticalSpacing6,
-        left: SpacingFoundation.horizontalSpacing12,
-        right: SpacingFoundation.horizontalSpacing12,
-      ),
+    return Stack(
+      children: [
+        UiKitCardWrapper(
+          width: width ?? 0.85.sw,
+          height: height ?? 0.24.sh,
+          color: color ?? theme?.colorScheme.inversePrimary,
+          borderRadius: BorderRadiusFoundation.all24r,
+          child: BarcodeWidget(
+            barcode: Barcode.code128(escapes: true),
+            data: barcodeNumber,
+            style: theme?.regularTextTheme.caption2.copyWith(color: theme.colorScheme.primary),
+            textPadding: SpacingFoundation.verticalSpacing8,
+          ).paddingOnly(
+            top: SpacingFoundation.verticalSpacing10,
+            bottom: SpacingFoundation.verticalSpacing6,
+            left: SpacingFoundation.horizontalSpacing12,
+            right: SpacingFoundation.horizontalSpacing12,
+          ),
+        ),
+        if (isBeenActivated)
+          UiKitCardWrapper(
+            color: ColorsFoundation.neutral48,
+            borderRadius: BorderRadiusFoundation.all24r,
+            child: SizedBox(
+              width: width ?? 0.85.sw,
+              height: height ?? 0.24.sh,
+            ),
+          ),
+      ],
     );
   }
 }
