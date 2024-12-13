99db04be08b2b797894167dcc7121a438dc20d34
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-12-13 21:21:38 +0300

    feat: add PhotoViewerPage

---
+++ lib/ui_kit/organisms/photo_viewer/photo_viewer_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+
+class PhotoViewerPage extends StatelessWidget {
+  PhotoViewerPage({super.key});
+
+  final List<String> images = [
+    GraphicsFoundation.instance.png.leto1.path,
+    GraphicsFoundation.instance.png.leto2.path,
+    GraphicsFoundation.instance.png.leto3.path,
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return BlurredAppBarPage(
+      title: 'Photo Gallery',
+      autoImplyLeading: true,
+      children: [
+        SizedBox(
+          width: 1.sw,
+          height: 1.sh,
+          child: GridView.builder(
+            padding: EdgeInsets.all(EdgeInsetsFoundation.all8),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 8.0,
+              mainAxisSpacing: 8.0,
+            ),
+            itemCount: images.length,
+            itemBuilder: (context, index) {
+              final String heroTag = 'photo-$index';
+
+              return GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(
+                    PopUpPhotoView(
+                      heroTag: heroTag,
+                      images: images,
+                      initialIndex: index,
+                    ),
+                  );
+                },
+                child: Hero(
+                  tag: heroTag,
+                  child: ImageWidget(
+                    link: images[index],
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
