f7f60878efa9f92e25912942328f342caf1cb046
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2024-12-18 18:28:33 +0300

    remove StatefulWidget, add ValueListenableBuilder for description

--- lib/ui_kit/atoms/cards/ui_kit_content_updates_card.dart
+++ lib/ui_kit/atoms/cards/ui_kit_content_updates_card.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
 
-class UiKitContentUpdatesCard extends StatefulWidget {
+class UiKitContentUpdatesCard extends StatelessWidget {
   final List<UiKitContentUpdateWidget> children;
   final String authorSpeciality;
   final String authorName;
@@ -26,7 +26,7 @@ class UiKitContentUpdatesCard extends StatefulWidget {
   final ValueNotifier<bool>? showTranslateButton;
   final ValueNotifier<String>? translateText;
 
-  const UiKitContentUpdatesCard({
+  UiKitContentUpdatesCard({
     super.key,
     required this.children,
     required this.authorSpeciality,
@@ -90,11 +90,6 @@ class UiKitContentUpdatesCard extends StatefulWidget {
         children: children,
       );
 
-  @override
-  State<UiKitContentUpdatesCard> createState() => _UiKitContentUpdatesCardState();
-}
-
-class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
   bool get showEmptyReactionsState =>
       heartCount == 0 && likeCount == 0 && sunglassesCount == 0 && fireCount == 0 && smileyCount == 0;
 
@@ -103,22 +98,20 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
     double reactionsSpacing = 0;
     double translateButtonSpacing = 0;
 
-    final currentText = description ?? '';
-
-    if (widget.hasGradientBorder && currentText.isNotEmpty) {
-      final linesCount = (currentText.length ~/ charactersPerLine) + currentText.split('\n').length;
+    if (hasGradientBorder && comment != null && comment!.isNotEmpty) {
+      final linesCount = (comment!.length ~/ charactersPerLine) + comment!.split('\n').length;
 
       additionalSpacingForComment += linesCount * (kIsWeb ? 15 : 18.h);
     }
-    if (widget.hasReactions) {
+    if (hasReactions) {
       reactionsSpacing += SpacingFoundation.verticalSpacing8 + SpacingFoundation.verticalSpacing20;
     }
-    if (widget.showTranslateButton != null && widget.showTranslateButton!.value) {
+    if (showTranslateButton != null && showTranslateButton!.value) {
       translateButtonSpacing += SpacingFoundation.verticalSpacing32;
     }
 
-    return widget.children.fold(0.0, (previousValue, element) => previousValue + element.height) +
-        ((widget.children.length + 2) * SpacingFoundation.verticalSpacing24) +
+    return children.fold(0.0, (previousValue, element) => previousValue + element.height) +
+        ((children.length + 2) * SpacingFoundation.verticalSpacing24) +
         (kIsWeb ? 17 : 0.152.sw) +
         EdgeInsetsFoundation.vertical24 +
         reactionsSpacing +
@@ -126,41 +119,17 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
         translateButtonSpacing;
   }
 
-  int get heartCount => widget.heartEyesReactionsCount ?? 0;
+  int get heartCount => heartEyesReactionsCount ?? 0;
 
-  int get likeCount => widget.likeReactionsCount ?? 0;
+  int get likeCount => likeReactionsCount ?? 0;
 
-  int get fireCount => widget.fireReactionsCount ?? 0;
+  int get fireCount => fireReactionsCount ?? 0;
 
-  int get sunglassesCount => widget.sunglassesReactionsCount ?? 0;
+  int get sunglassesCount => sunglassesReactionsCount ?? 0;
 
-  int get smileyCount => widget.smileyReactionsCount ?? 0;
+  int get smileyCount => smileyReactionsCount ?? 0;
 
   int get charactersPerLine => 1.sw >= 380 ? 38 : 33;
-
-  bool isTranslate = false;
-
-  late String? description;
-
-  @override
-  void initState() {
-    super.initState();
-
-    description = widget.comment;
-    widget.showTranslateButton?.addListener(_updateHeight);
-  }
-
-  @override
-  void dispose() {
-    widget.showTranslateButton?.removeListener(_updateHeight);
-    super.dispose();
-  }
-
-  void _updateHeight() {
-    setState(() {
-      overallHeight();
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -174,8 +143,16 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
     bool isOverlayVisible = false;
     OverlayEntry? overlayEntry;
 
+    final description = ValueNotifier<String>(comment ?? '');
+    final isTranslate = ValueNotifier<bool>(false);
+
+    void toggleTranslation() {
+      isTranslate.value = !isTranslate.value;
+      description.value = isTranslate.value ? (translateText?.value ?? comment ?? '') : comment ?? '';
+    }
+
     _children() {
-      return widget.hasGradientBorder
+      return hasGradientBorder
           ? Column(
               mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -183,10 +160,10 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
                 SizedBox(
                   width: kIsWeb ? 90 : 1.sw,
                   child: UiKitShuffleTile(
-                    trailing: widget.onSharePress != null
+                    trailing: onSharePress != null
                         ? context.iconButtonNoPadding(
                             data: BaseUiKitButtonData(
-                                onPressed: widget.onSharePress,
+                                onPressed: onSharePress,
                                 iconInfo: BaseUiKitButtonIconData(
                                   iconData: ShuffleUiKitIcons.share,
                                   color: colorScheme?.darkNeutral800,
@@ -194,45 +171,37 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
                         : null,
                   ),
                 ),
-                if (widget.hasGradientBorder && description != null && description!.isNotEmpty)
-                  Text(
-                    description!,
-                    style: regularTextTheme?.caption2,
-                    textAlign: TextAlign.start,
-                  ).paddingOnly(top: EdgeInsetsFoundation.vertical16),
-                if (widget.children.isNotEmpty) ...[
+                if (hasGradientBorder && description.value.isNotEmpty)
+                  ValueListenableBuilder<String>(
+                    valueListenable: description,
+                    builder: (_, desc, __) => Text(
+                      desc,
+                      style: regularTextTheme?.caption2,
+                      textAlign: TextAlign.start,
+                    ).paddingOnly(top: EdgeInsetsFoundation.vertical16),
+                  ),
+                if (children.isNotEmpty) ...[
                   SpacingFoundation.verticalSpace4,
-                  ...widget.children.map((child) {
-                    final isLastChild = widget.children.indexOf(child) == widget.children.length - 1;
+                  ...children.map((child) {
+                    final isLastChild = children.indexOf(child) == children.length - 1;
 
                     if (!isLastChild) return child.paddingOnly(bottom: EdgeInsetsFoundation.vertical16);
                     return child;
                   }),
                 ],
                 SpacingFoundation.verticalSpace8,
-                if (widget.showTranslateButton != null)
+                if (showTranslateButton != null)
                   Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     mainAxisSize: MainAxisSize.max,
                     children: [
-                      ListenableBuilder(
-                        listenable: widget.showTranslateButton!,
-                        builder: (context, child) => InkWell(
-                          onTap: () {
-                            setState(() {
-                              isTranslate = !isTranslate;
-                              if (isTranslate) {
-                                if (widget.translateText?.value != null && widget.translateText!.value.isNotEmpty) {
-                                  description = widget.translateText?.value;
-                                }
-                              } else {
-                                description = widget.comment;
-                              }
-                            });
-                          },
-                          child: widget.showTranslateButton!.value
+                      ValueListenableBuilder<bool>(
+                        valueListenable: isTranslate,
+                        builder: (_, isTranslating, __) => InkWell(
+                          onTap: toggleTranslation,
+                          child: showTranslateButton!.value
                               ? Text(
-                                  isTranslate ? S.of(context).Original : S.of(context).Translate,
+                                  isTranslating ? S.of(context).Original : S.of(context).Translate,
                                   style: context.uiKitTheme?.regularTextTheme.caption4Regular.copyWith(
                                     color: isLightTheme
                                         ? ColorsFoundation.darkNeutral700
@@ -244,7 +213,7 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
                       ),
                     ],
                   ).paddingOnly(bottom: SpacingFoundation.verticalSpacing8),
-                if (widget.hasReactions)
+                if (hasReactions)
                   Row(
                     mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -255,21 +224,21 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
                         padding: EdgeInsets.symmetric(
                             horizontal: SpacingFoundation.horizontalSpacing4,
                             vertical: SpacingFoundation.verticalSpacing2),
-                        child: Text(widget.createdAt, style: regularTextTheme?.caption3),
+                        child: Text(createdAt, style: regularTextTheme?.caption3),
                       ),
                       showEmptyReactionsState
                           ? Builder(
                               builder: (c) => TapRegion(
                                 behavior: HitTestBehavior.opaque,
                                 onTapInside: (value) {
-                                  if (widget.onReactionsTapped != null) {
+                                  if (onReactionsTapped != null) {
                                     isOverlayVisible
                                         ? hideReactionOverlay(overlayEntry)
                                         : showReactionOverlay(
                                             c,
                                             overlayEntry,
                                             reactionTextColor,
-                                            widget.onReactionsTapped,
+                                            onReactionsTapped,
                                           );
                                     isOverlayVisible = !isOverlayVisible;
                                   }
@@ -288,14 +257,14 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
                               builder: (c) => TapRegion(
                                 behavior: HitTestBehavior.opaque,
                                 onTapInside: (value) {
-                                  if (widget.onReactionsTapped != null) {
+                                  if (onReactionsTapped != null) {
                                     isOverlayVisible
                                         ? hideReactionOverlay(overlayEntry)
                                         : showReactionOverlay(
                                             c,
                                             overlayEntry,
                                             reactionTextColor,
-                                            widget.onReactionsTapped,
+                                            onReactionsTapped,
                                           );
                                     isOverlayVisible = !isOverlayVisible;
                                   }
@@ -351,16 +320,16 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
               children: [
                 context.userTile(
                   data: BaseUiKitUserTileData(
-                    name: widget.authorName,
-                    username: widget.authorUsername,
-                    avatarUrl: widget.authorAvatarUrl,
-                    type: widget.authorUserType,
+                    name: authorName,
+                    username: authorUsername,
+                    avatarUrl: authorAvatarUrl,
+                    type: authorUserType,
                     noMaterialOverlay: true,
                   ),
                 ),
                 SpacingFoundation.verticalSpace8,
-                ...widget.children.map((child) {
-                  final isLastChild = widget.children.indexOf(child) == widget.children.length - 1;
+                ...children.map((child) {
+                  final isLastChild = children.indexOf(child) == children.length - 1;
 
                   if (!isLastChild) return child.paddingOnly(bottom: EdgeInsetsFoundation.vertical16);
                   return child;
@@ -377,7 +346,7 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
           intensities: [130, 170],
           pattern: [10, 5],
         ));
-        widget.onLongPress?.call();
+        onLongPress?.call();
       },
       child: ConstrainedBox(
         constraints: BoxConstraints(
@@ -401,7 +370,7 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
                     color: isLightTheme ? colorScheme?.surface2 : null,
                     gradient: isLightTheme ? null : GradientFoundation.shunyGreyGradientInverted,
                     borderRadius: BorderRadiusFoundation.all24,
-                    border: widget.hasGradientBorder ? GradientFoundation.touchIdgradientBorder : null,
+                    border: hasGradientBorder ? GradientFoundation.touchIdgradientBorder : null,
                   ),
                   child: BackdropFilter(
                     filter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
@@ -414,7 +383,7 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
                   ),
                 )),
             _children(),
-            if (widget.authorSpeciality.isNotEmpty)
+            if (authorSpeciality.isNotEmpty)
               Positioned(
                 right: 0,
                 bottom: -SpacingFoundation.verticalSpacing8,
@@ -425,7 +394,7 @@ class _UiKitContentUpdatesCardState extends State<UiKitContentUpdatesCard> {
                     child: ColoredBox(
                       color: isLightTheme ? ColorsFoundation.darkNeutral300 : ColorsFoundation.neutral16,
                       child: Text(
-                        widget.authorSpeciality,
+                        authorSpeciality,
                         style: boldTextTheme?.caption3Medium.copyWith(
                           color: isLightTheme ? colorScheme?.darkNeutral800 : colorScheme?.darkNeutral100,
                         ),
