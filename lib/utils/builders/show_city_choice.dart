8086ed33b6fcc6cdd5466a9e373be6ba828f23cd
romancores <225000@niuitmo.ru>
2025-01-08 15:38:13 -0500

    SFK-614

--- lib/utils/builders/show_city_choice.dart
+++ lib/utils/builders/show_city_choice.dart
@@ -5,7 +5,7 @@ void showCitiesOverlay({
   required BuildContext context,
   OverlayEntry? overlayEntry,
   ValueChanged<String>? onCityTapped,
-  List<String>? cities,
+  List<String> cities = const [],
   String? selectedCity,
 }) {
   final theme = context.uiKitTheme;
@@ -14,66 +14,65 @@ void showCitiesOverlay({
 
   overlayEntry = OverlayEntry(
     builder: (context) => Positioned(
-      top: (position.dy / 2) + 10.h,
-      right: 10.w,
-      child: Material(
-        color: Colors.transparent,
-        child: Container(
-          decoration: BoxDecoration(
-            color: theme?.colorScheme.surface5,
-            borderRadius: BorderRadiusFoundation.all16,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.white.withOpacity(0.3),
-                blurRadius: 5.0,
-                spreadRadius: 5.0,
-              ),
-            ],
-          ),
-          padding: EdgeInsets.symmetric(
-            horizontal: SpacingFoundation.horizontalSpacing24,
-            vertical: SpacingFoundation.verticalSpacing12,
-          ),
-          child: TapRegion(
-            onTapOutside: (event) {
-              hideCitiesOverlay(overlayEntry);
-            },
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                if (cities != null && cities.isNotEmpty)
-                  ...cities.map(
-                    (city) {
-                      return InkWell(
-                        onTap: () {
-                          onCityTapped?.call(city);
-                          hideCitiesOverlay(overlayEntry);
-                        },
-                        child: Row(
-                          children: [
-                            Visibility(
-                              visible: selectedCity == city,
-                              maintainSize: true,
-                              maintainAnimation: true,
-                              maintainState: true,
-                              child: const UiKitRadio(selected: true)
-                                  .paddingOnly(right: SpacingFoundation.horizontalSpacing12),
-                            ),
-                            Text(
-                              city,
-                              style: theme?.boldTextTheme.labelLarge,
-                            ),
-                          ],
-                        ).paddingSymmetric(vertical: SpacingFoundation.verticalSpacing12),
-                      );
-                    },
-                  ),
+        top: (position.dy / 2) + 10.h,
+        right: 10.w,
+        child: Material(
+          color: Colors.transparent,
+          child: Container(
+            decoration: BoxDecoration(
+              color: theme?.colorScheme.surface5,
+              borderRadius: BorderRadiusFoundation.all16,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.white.withOpacity(0.3),
+                  blurRadius: 5.0,
+                  spreadRadius: 5.0,
+                ),
               ],
             ),
+            padding: EdgeInsets.symmetric(
+              horizontal: SpacingFoundation.horizontalSpacing24,
+              vertical: SpacingFoundation.verticalSpacing12,
+            ),
+            child: TapRegion(
+              onTapOutside: (event) {
+                hideCitiesOverlay(overlayEntry);
+              },
+              child: SizedBox(
+                  height: 0.5.sh,
+                  width: 150,
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: cities.map(
+                      (city) {
+                        return InkWell(
+                          onTap: () {
+                            onCityTapped?.call(city);
+                            hideCitiesOverlay(overlayEntry);
+                          },
+                          child: Row(
+                            children: [
+                              Visibility(
+                                visible: selectedCity == city,
+                                maintainSize: true,
+                                maintainAnimation: true,
+                                maintainState: true,
+                                child: const UiKitRadio(selected: true)
+                                    .paddingOnly(right: SpacingFoundation.horizontalSpacing12),
+                              ),
+                              Text(
+                                city,
+                                style: theme?.boldTextTheme.labelLarge,
+                              ),
+                            ],
+                          ).paddingSymmetric(vertical: SpacingFoundation.verticalSpacing12),
+                        );
+                      },
+                    ).toList(),
+                  )),
+            ),
           ),
-        ),
-      ),
-    ),
+        )),
   );
   Overlay.of(context).insert(overlayEntry);
 }
