c65ac8eb0bbd01ee7491199f800bbdde592ad85b
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-01-17 18:21:30 +0300

    remove UiKitCardWrapper

--- lib/ui_kit/atoms/cards/digest_content_card.dart
+++ lib/ui_kit/atoms/cards/digest_content_card.dart
@@ -12,96 +12,79 @@ class DigestContentCard extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = context.uiKitTheme;
     final boldTextTheme = theme?.boldTextTheme;
-    final regularTextTheme = theme?.regularTextTheme;
     final colorScheme = theme?.colorScheme;
 
-    return UiKitCardWrapper(
-      color: colorScheme?.surface3,
-      padding: EdgeInsets.all(EdgeInsetsFoundation.all12),
-      borderRadius: BorderRadiusFoundation.all16,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadiusFoundation.all8,
-                child: ImageWidget(
-                  height: 40.w,
-                  width: 50.w,
-                  link: digestUiModel?.imageUrl,
-                  fit: BoxFit.cover,
-                ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadiusFoundation.all8,
+              child: ImageWidget(
+                height: 40.w,
+                width: 50.w,
+                link: digestUiModel?.imageUrl,
+                fit: BoxFit.cover,
               ),
-              SpacingFoundation.horizontalSpace8,
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  if (digestUiModel?.title != null && digestUiModel!.title!.isNotEmpty)
-                    SizedBox(
-                      width: 175.w,
-                      child: Text(
-                        digestUiModel!.title!,
-                        style: boldTextTheme?.caption3Medium,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                  SpacingFoundation.verticalSpace4,
-                  SizedBox(
-                    height: 15.w,
-                    width: 170.w,
-                    child: ListView(
-                      physics: const BouncingScrollPhysics(),
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      children: [
-                        if (digestUiModel?.rating != null)
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ImageWidget(
-                                iconData: ShuffleUiKitIcons.starfill,
-                                color: colorScheme?.inverseSurface,
-                                height: 0.040625.sw,
-                                width: 0.040625.sw,
-                                fit: BoxFit.cover,
-                              ),
-                              SpacingFoundation.horizontalSpace2,
-                              Text(
-                                '${digestUiModel!.rating}',
-                                style: boldTextTheme?.caption3Medium,
-                              ).paddingOnly(top: 2.w),
-                              SpacingFoundation.horizontalSpace12,
-                            ],
-                          ),
-                        if (digestUiModel?.tags != null && digestUiModel!.tags!.isNotEmpty)
-                          ...digestUiModel!.tags!.map(
-                            (e) => UiKitTagWidget(
-                              title: e.title,
-                              icon: e.icon,
-                            ).paddingOnly(right: SpacingFoundation.horizontalSpacing12),
-                          ),
-                      ],
-                    ),
-                  )
-                ],
-              ).paddingSymmetric(vertical: 5.h)
-            ],
-          ),
-          if (digestUiModel?.contentDescriptionNotifier != null &&
-              digestUiModel!.contentDescriptionNotifier!.value.isNotEmpty)
-            ValueListenableBuilder<String>(
-              valueListenable: digestUiModel!.contentDescriptionNotifier!,
-              builder: (_, contentDescriptionTranslate, __) => Text(
-                contentDescriptionTranslate,
-                style: regularTextTheme?.caption4Regular,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-              ).paddingOnly(top: SpacingFoundation.verticalSpacing8),
             ),
-        ],
-      ),
+            SpacingFoundation.horizontalSpace8,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                if (digestUiModel?.title != null && digestUiModel!.title!.isNotEmpty)
+                  SizedBox(
+                    width: 175.w,
+                    child: Text(
+                      digestUiModel!.title!,
+                      style: boldTextTheme?.caption3Medium,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ),
+                SpacingFoundation.verticalSpace4,
+                SizedBox(
+                  height: 15.w,
+                  width: 170.w,
+                  child: ListView(
+                    physics: const BouncingScrollPhysics(),
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      if (digestUiModel?.rating != null)
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ImageWidget(
+                              iconData: ShuffleUiKitIcons.starfill,
+                              color: colorScheme?.inverseSurface,
+                              height: 0.040625.sw,
+                              width: 0.040625.sw,
+                              fit: BoxFit.cover,
+                            ),
+                            SpacingFoundation.horizontalSpace2,
+                            Text(
+                              '${digestUiModel!.rating}',
+                              style: boldTextTheme?.caption3Medium,
+                            ).paddingOnly(top: 2.w),
+                            SpacingFoundation.horizontalSpace12,
+                          ],
+                        ),
+                      if (digestUiModel?.tags != null && digestUiModel!.tags!.isNotEmpty)
+                        ...digestUiModel!.tags!.map(
+                          (e) => UiKitTagWidget(
+                            title: e.title,
+                            icon: e.icon,
+                          ).paddingOnly(right: SpacingFoundation.horizontalSpacing12),
+                        ),
+                    ],
+                  ),
+                )
+              ],
+            ).paddingSymmetric(vertical: 5.h)
+          ],
+        ),
+      ],
     );
   }
 }
