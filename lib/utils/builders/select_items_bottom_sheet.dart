d06452dd9d14c4374ac17c36a16ff6c3832bc8ea
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-02-11 18:02:08 +0300

    fix: naming

---
+++ lib/utils/builders/select_items_bottom_sheet.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+
+selectItemsBottomSheet(
+  BuildContext context, {
+  ValueChanged<String>? onSelectItem,
+  List<String?>? items,
+  String? selectedItem,
+  String? title,
+}) {
+  final theme = context.uiKitTheme;
+  final boldTextTheme = theme?.boldTextTheme;
+  final itemCount = items?.length ?? 1;
+
+  //TODO for big device
+  final topPadding = itemCount <= 2
+      ? 0.55.sh
+      : itemCount <= 3
+          ? 0.45.sh
+          : itemCount <= 4
+              ? 0.35.sh
+              : 0.3.sh;
+
+  return showUiKitGeneralFullScreenDialog(
+    context,
+    GeneralDialogData(
+      isWidgetScrollable: true,
+      topPadding: topPadding,
+      child: Column(
+        children: [
+          Text(
+            title ?? S.of(context).SelectCity,
+            style: boldTextTheme?.subHeadline,
+          ).paddingAll(EdgeInsetsFoundation.all16),
+          if (items == null || items.isEmpty)
+            Center(
+              child: Text(
+                S.of(context).NothingFound,
+                style: boldTextTheme?.caption1Bold,
+              ),
+            )
+          else
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+                itemCount: itemCount,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {
+                      onSelectItem?.call(items[index] ?? '');
+                      context.pop();
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if (index == 0)
+                          Divider(thickness: 2.h, color: theme?.colorScheme.surface2).paddingOnly(
+                            bottom: SpacingFoundation.verticalSpacing16,
+                          ),
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            if (selectedItem != null && selectedItem.isNotEmpty)
+                              UiKitRadio(
+                                selected: items[index] == selectedItem,
+                              ).paddingOnly(right: SpacingFoundation.horizontalSpacing8),
+                            Flexible(
+                              child: Text(
+                                items[index] ?? '',
+                                style: boldTextTheme?.caption1Medium,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Divider(thickness: 2.h, color: theme?.colorScheme.surface2).paddingOnly(
+                          top: SpacingFoundation.verticalSpacing16,
+                        ),
+                      ],
+                    ).paddingOnly(
+                      bottom: SpacingFoundation.verticalSpacing16,
+                      left: SpacingFoundation.horizontalSpacing16,
+                      right: SpacingFoundation.horizontalSpacing16,
+                    ),
+                  );
+                },
+              ).paddingSymmetric(vertical: SpacingFoundation.verticalSpacing16),
+            )
+        ],
+      ),
+    ),
+  );
+}
