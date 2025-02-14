335e16062b540ad8f5efc8e204267c4e769b162c
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-02-14 12:30:23 +0300

    add ValueNotifier for reactionStatisticUiModel

--- lib/ui_kit/atoms/reactions/ui_kit_column_statistic_reaction.dart
+++ lib/ui_kit/atoms/reactions/ui_kit_column_statistic_reaction.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
 
 class UiKitColumnStatisticReaction extends StatelessWidget {
-  final ReactionStatisticUiModel? reactionStatisticUiModel;
+  final ValueNotifier<ReactionStatisticUiModel?>? reactionStatisticUiModel;
 
   const UiKitColumnStatisticReaction({
     super.key,
@@ -14,78 +14,72 @@ class UiKitColumnStatisticReaction extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textStyle = context.uiKitTheme?.regularTextTheme.caption4Semibold;
-
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        if (reactionStatisticUiModel!.viewCount != null)
-          ValueListenableBuilder(
-            valueListenable: reactionStatisticUiModel!.viewCount!,
-            builder: (_, viewCount, __) {
-              return (viewCount != null && viewCount > 0)
-                  ? ClipRRect(
-                      borderRadius: BorderRadiusFoundation.all12,
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              '${viewCount}',
-                              style: textStyle,
+    return reactionStatisticUiModel != null
+        ? ValueListenableBuilder(
+            valueListenable: reactionStatisticUiModel!,
+            builder: (_, value, __) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  (value?.viewCount != null && value!.viewCount! > 0)
+                      ? ClipRRect(
+                          borderRadius: BorderRadiusFoundation.all12,
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  '${value.viewCount}',
+                                  style: textStyle,
+                                ),
+                                SpacingFoundation.horizontalSpace8,
+                                ImageWidget(
+                                  height: 16.w,
+                                  width: 16.w,
+                                  iconData: ShuffleUiKitIcons.view,
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ).paddingSymmetric(
+                              horizontal: SpacingFoundation.horizontalSpacing8,
+                              vertical: SpacingFoundation.verticalSpacing4,
                             ),
-                            SpacingFoundation.horizontalSpace8,
-                            ImageWidget(
-                              height: 16.w,
-                              width: 16.w,
-                              iconData: ShuffleUiKitIcons.view,
-                              color: Colors.white,
+                          ),
+                        ).paddingOnly(bottom: SpacingFoundation.verticalSpacing12)
+                      : SizedBox.shrink(),
+                  (value?.likeCount != null && value!.likeCount! > 0)
+                      ? ClipRRect(
+                          borderRadius: BorderRadiusFoundation.all12,
+                          child: BackdropFilter(
+                            filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  '${value.likeCount}',
+                                  style: textStyle,
+                                ),
+                                SpacingFoundation.horizontalSpace8,
+                                ImageWidget(
+                                  height: 16.w,
+                                  width: 16.w,
+                                  iconData: ShuffleUiKitIcons.heartoutline,
+                                  color: Colors.white,
+                                ),
+                              ],
+                            ).paddingSymmetric(
+                              horizontal: SpacingFoundation.horizontalSpacing8,
+                              vertical: SpacingFoundation.verticalSpacing4,
                             ),
-                          ],
-                        ).paddingSymmetric(
-                          horizontal: SpacingFoundation.horizontalSpacing8,
-                          vertical: SpacingFoundation.verticalSpacing4,
-                        ),
-                      ),
-                    ).paddingOnly(bottom: SpacingFoundation.verticalSpacing12)
-                  : SizedBox.shrink();
-            },
-          ),
-        if (reactionStatisticUiModel?.likeCount?.value != null)
-          ValueListenableBuilder(
-            valueListenable: reactionStatisticUiModel!.likeCount!,
-            builder: (_, likeCount, __) {
-              return (likeCount != null && likeCount > 0)
-                  ? ClipRRect(
-                      borderRadius: BorderRadiusFoundation.all12,
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 25, sigmaY: 25),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              '${likeCount}',
-                              style: textStyle,
-                            ),
-                            SpacingFoundation.horizontalSpace8,
-                            ImageWidget(
-                              height: 16.w,
-                              width: 16.w,
-                              iconData: ShuffleUiKitIcons.heartoutline,
-                              color: Colors.white,
-                            ),
-                          ],
-                        ).paddingSymmetric(
-                          horizontal: SpacingFoundation.horizontalSpacing8,
-                          vertical: SpacingFoundation.verticalSpacing4,
-                        ),
-                      ),
-                    )
-                  : SizedBox.shrink();
+                          ),
+                        )
+                      : SizedBox.shrink()
+                ],
+              );
             },
           )
-      ],
-    );
+        : SizedBox.shrink();
   }
 }
