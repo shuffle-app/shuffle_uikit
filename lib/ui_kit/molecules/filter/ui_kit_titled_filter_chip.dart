c70a694f8177ba7d5b0955ab634ac6c99ba85ca3
romancores <225000@niuitmo.ru>
2025-02-17 17:15:06 +0800

    SFK-697

--- lib/ui_kit/molecules/filter/ui_kit_titled_filter_chip.dart
+++ lib/ui_kit/molecules/filter/ui_kit_titled_filter_chip.dart
@@ -4,54 +4,82 @@ import 'package:shuffle_uikit/shuffle_uikit.dart';
 class UiKitTitledFilterChip extends StatelessWidget {
   final String title;
   final bool selected;
+  final bool loading;
 
   final VoidCallback? onPressed;
   final dynamic icon;
 
-  const UiKitTitledFilterChip({
-    super.key,
-    required this.title,
-    this.selected = false,
-    this.onPressed,
-    required this.icon,
-  });
+  const UiKitTitledFilterChip(
+      {super.key,
+      required this.title,
+      this.selected = false,
+      this.onPressed,
+      required this.icon,
+      this.loading = false});
 
   @override
   Widget build(BuildContext context) {
-    final titleStyle = context.uiKitTheme?.boldTextTheme.caption1Bold;
-    final colorScheme = context.uiKitTheme?.colorScheme;
+    final theme = context.uiKitTheme;
+    final titleStyle = theme?.boldTextTheme.caption1Bold;
+    final colorScheme = theme?.colorScheme;
+    final background = selected ? colorScheme?.inversePrimary : colorScheme?.surface2;
+    final foreground = selected ? colorScheme?.primary : colorScheme?.inversePrimary;
+    final textStyle = titleStyle?.copyWith(
+      color: foreground,
+    );
+    final TextSpan textSpan = TextSpan(
+      text: title,
+      style: textStyle,
+    );
+    final textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+    );
 
-    return Material(
+    textPainter.layout();
+
+    final loadingWidth = textPainter.width;
+
+    return InkWell(
       borderRadius: BorderRadiusFoundation.max,
-      color: selected ? colorScheme?.inversePrimary : colorScheme?.surface2,
-      clipBehavior: Clip.hardEdge,
-      child: InkWell(
-        onTap: onPressed,
-        child: Ink(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Transform.translate(
-                  offset: Offset(0, icon is IconData ? -2.h : 0),
-                  child: ImageWidget(
-                    fit: BoxFit.fitHeight,
-                    iconData: icon is IconData ? icon as IconData : null,
-                    link: icon is String ? icon as String : null,
-                    color: selected ? colorScheme?.primary : colorScheme?.inversePrimary,
-                  )),
-              SpacingFoundation.horizontalSpace8,
-              Text(
-                title,
-                style: titleStyle?.copyWith(
-                  color: selected ? colorScheme?.primary : colorScheme?.inversePrimary,
-                ),
-              ),
-            ],
-          ).paddingSymmetric(
+      onTap: onPressed,
+      child: UiKitCardWrapper(
+          borderRadius: BorderRadiusFoundation.max,
+          color: background,
+          clipBehavior: Clip.hardEdge,
+          padding: EdgeInsets.symmetric(
               horizontal: SpacingFoundation.horizontalSpacing16, vertical: SpacingFoundation.verticalSpacing14),
-        ),
-      ),
+          child: loading
+              ? SizedBox(
+                  width: SpacingFoundation.horizontalSpacing8 * 2 +
+                      (SpacingFoundation.horizontalSpacing16 * 2) +
+                      loadingWidth,
+                  child: Center(
+                    child: SizedBox(
+                      width: 40.h,
+                      child: CircularProgressIndicator.adaptive(
+                        valueColor: AlwaysStoppedAnimation(foreground),
+                        backgroundColor: Colors.transparent,
+                      ),
+                    ),
+                  ),
+                )
+              : Row(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Transform.translate(
+                        offset: Offset(0, icon is IconData ? -2.h : 0),
+                        child: ImageWidget(
+                          fit: BoxFit.fitHeight,
+                          iconData: icon is IconData ? icon as IconData : null,
+                          link: icon is String ? icon as String : null,
+                          color: foreground,
+                        )),
+                    SpacingFoundation.horizontalSpace8,
+                    Text(title, style: textStyle),
+                  ],
+                )),
     );
   }
 }
