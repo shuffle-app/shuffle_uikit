f013cbf85862ee9a1371df2fb88851146897e235
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-02-21 09:23:35 +0300

    feat: add UiKitSecondaryBackgroundDismissible

---
+++ lib/ui_kit/molecules/dismissible/ui_kit_secondary_background_dismissible.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+
+class UiKitSecondaryBackgroundDismissible extends StatelessWidget {
+  const UiKitSecondaryBackgroundDismissible({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = context.uiKitTheme;
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: ColorsFoundation.red,
+        borderRadius: BorderRadiusFoundation.all24r,
+      ),
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            context.iconButtonNoPadding(
+              data: BaseUiKitButtonData(
+                iconInfo: BaseUiKitButtonIconData(
+                  iconData: ShuffleUiKitIcons.trash,
+                ),
+              ),
+            ),
+            SpacingFoundation.horizontalSpace2,
+            Text(
+              S.of(context).Remove,
+              style: theme?.boldTextTheme.caption2Medium,
+            ),
+            SpacingFoundation.horizontalSpace12,
+          ],
+        ),
+      ),
+    );
+  }
+}
