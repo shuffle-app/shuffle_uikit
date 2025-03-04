c7050af29f7752ad2c69ca5e795f19a26bd7809b
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-03-04 10:25:23 +0300

    remove isLoading

--- lib/utils/builders/create_photo_dialog.dart
+++ lib/utils/builders/create_photo_dialog.dart
@@ -6,7 +6,6 @@ Future<void> createPhotoDialog(
   required TextEditingController controller,
   VoidCallback? onYesTap,
   VoidCallback? onCancelTap,
-  bool isLoading = false,
 }) async {
   final theme = context.uiKitTheme;
   final colorsScheme = theme?.colorScheme;
@@ -24,62 +23,58 @@ Future<void> createPhotoDialog(
         ),
         textAlign: TextAlign.center,
       ),
-      content: isLoading
-          ? CircularProgressIndicator.adaptive()
-          : Form(
-              key: formKey,
-              child: SizedBox(
-                width: 0.75.sw,
-                child: UiKitInputFieldNoFill(
-                  controller: controller,
-                  label: S.current.Title,
-                  maxSymbols: 25,
-                  customLabelColor: colorsScheme?.inverseBodyTypography,
-                  customEnabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: colorsScheme?.darkNeutral400 ?? ColorsFoundation.darkNeutral400,
-                    ),
-                  ),
-                  customFocusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: colorsScheme?.darkNeutral400 ?? ColorsFoundation.darkNeutral400,
-                    ),
-                  ),
-                  customInputTextColor: colorsScheme?.inverseHeadingTypography,
-                  cursorColor: colorsScheme?.inverseHeadingTypography,
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return S.current.AddTitle;
-                    }
-                    return null;
-                  },
-                  onChanged: (value) {
-                    formKey.currentState?.validate();
-                  },
-                ),
+      content: Form(
+        key: formKey,
+        child: SizedBox(
+          width: 0.75.sw,
+          child: UiKitInputFieldNoFill(
+            controller: controller,
+            label: S.current.Title,
+            maxSymbols: 25,
+            customLabelColor: colorsScheme?.inverseBodyTypography,
+            customEnabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: colorsScheme?.darkNeutral400 ?? ColorsFoundation.darkNeutral400,
               ),
             ),
-      actions: isLoading
-          ? null
-          : [
-              context.dialogButton(
-                small: true,
-                dialogButtonType: DialogButtonType.buttonBlack,
-                data: BaseUiKitButtonData(
-                  text: S.of(context).Yes,
-                  onPressed: () {
-                    if (formKey.currentState?.validate() ?? false) onYesTap?.call();
-                  },
-                ),
+            customFocusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(
+                color: colorsScheme?.darkNeutral400 ?? ColorsFoundation.darkNeutral400,
               ),
-              context.smallButton(
-                data: BaseUiKitButtonData(
-                  text: S.current.Cancel,
-                  onPressed: onCancelTap,
-                  backgroundColor: Colors.transparent,
-                ),
-              )
-            ],
+            ),
+            customInputTextColor: colorsScheme?.inverseHeadingTypography,
+            cursorColor: colorsScheme?.inverseHeadingTypography,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return S.current.AddTitle;
+              }
+              return null;
+            },
+            onChanged: (value) {
+              formKey.currentState?.validate();
+            },
+          ),
+        ),
+      ),
+      actions: [
+        context.dialogButton(
+          small: true,
+          dialogButtonType: DialogButtonType.buttonBlack,
+          data: BaseUiKitButtonData(
+            text: S.of(context).Yes,
+            onPressed: () {
+              if (formKey.currentState?.validate() ?? false) onYesTap?.call();
+            },
+          ),
+        ),
+        context.smallButton(
+          data: BaseUiKitButtonData(
+            text: S.current.Cancel,
+            onPressed: onCancelTap,
+            backgroundColor: Colors.transparent,
+          ),
+        )
+      ],
     ),
   );
 }
