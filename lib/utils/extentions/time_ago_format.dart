09a7034c2abc0e882d08bd42c84d2eeec6895fa1
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-03-11 16:21:11 +0300

    fix: text

--- lib/utils/extentions/time_ago_format.dart
+++ lib/utils/extentions/time_ago_format.dart
@@ -11,17 +11,17 @@ extension TimeFormatterExtension on DateTime {
 
     if (difference.inDays >= 365) {
       final years = (difference.inDays / 365).floor();
-      return '$years ${S.current.YearsAgoX(years)}';
+      return S.current.YearsAgoX(years);
     } else if (difference.inDays >= 30) {
       final months = (difference.inDays / 30).floor();
-      return '$months ${S.current.MouthsAgoX(months)}';
+      return S.current.MouthsAgoX(months);
     } else if (difference.inDays >= 7) {
       final weeks = (difference.inDays / 7).floor();
-      return '$weeks ${S.current.WeeksAgoX(weeks)}';
+      return S.current.WeeksAgoX(weeks);
     } else if (difference.inDays >= 1) {
-      return '${difference.inDays} ${S.current.DaysAgoX(difference.inDays)}';
+      return S.current.DaysAgoX(difference.inDays);
     } else if (difference.inHours >= 1) {
-      return '${difference.inHours} ${S.current.HoursAgoX(difference.inHours)}';
+      return S.current.HoursAgoX(difference.inHours);
     } else if (difference.inMinutes >= 1) {
       return '${difference.inMinutes} ${S.current.MinAgoX}';
     } else if (difference.inSeconds >= 10) {
