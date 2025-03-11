d76aa2194ee98259874c2fda68f32495d1c5fdd6
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-03-11 12:59:47 +0300

    fix: naming

--- lib/localization/l10n.dart
+++ lib/localization/l10n.dart
@@ -28,7 +28,10 @@ class S {
   static const AppLocalizationDelegate delegate = AppLocalizationDelegate();
 
   static Future<S> load(Locale locale) {
-    final name = (locale.countryCode?.isEmpty ?? false) ? locale.languageCode : locale.toString();
+    final name =
+        (locale.countryCode?.isEmpty ?? false)
+            ? locale.languageCode
+            : locale.toString();
     final localeName = Intl.canonicalizedLocale(name);
     return initializeMessages(localeName).then((_) {
       Intl.defaultLocale = localeName;
@@ -6796,7 +6799,8 @@ class S {
       one: 'locked sections & functions',
       two: 'points, rewards & money',
       few: 'invites, chats, hang outs',
-      other: 'Premium, PRO & organisation, Influencer accounts (use & create content)',
+      other:
+          'Premium, PRO & organisation, Influencer accounts (use & create content)',
       name: 'LoginBenefits',
       desc: '',
       args: [count],
@@ -9195,6 +9199,21 @@ class S {
       desc: '',
       args: [],
     );
+  }
+
+  /// `Your TOPs`
+  String get YourTOPs {
+    return Intl.message('Your TOPs', name: 'YourTOPs', desc: '', args: []);
+  }
+
+  /// `Add new`
+  String get AddNew {
+    return Intl.message('Add new', name: 'AddNew', desc: '', args: []);
+  }
+
+  /// `New TOP`
+  String get NewTOP {
+    return Intl.message('New TOP', name: 'NewTOP', desc: '', args: []);
   }
 }
 
