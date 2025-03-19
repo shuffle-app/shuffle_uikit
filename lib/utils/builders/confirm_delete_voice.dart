d3010c43f89be8233558188720e3c18b015cc6a0
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2025-03-19 12:49:58 +0300

    feat: confirmDeleteVoice

---
+++ lib/utils/builders/confirm_delete_voice.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:shuffle_uikit/shuffle_uikit.dart';
+
+confirmDeleteVoice(
+  BuildContext context, {
+  VoidCallback? onYesTap,
+  VoidCallback? onCancelTap,
+}) async {
+  final theme = context.uiKitTheme;
+
+  await showUiKitAlertDialog(
+    context,
+    AlertDialogData(
+      insetPadding: EdgeInsets.all(EdgeInsetsFoundation.all32),
+      title: Text(
+        S.of(context).AreYouSureYouWantToDeleteThisVoice,
+        style: theme?.boldTextTheme.title2.copyWith(
+          color: theme.colorScheme.inverseBodyTypography,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      defaultButtonText: '',
+      actions: [
+        context.dialogButton(
+          small: true,
+          dialogButtonType: DialogButtonType.buttonRed,
+          data: BaseUiKitButtonData(
+            text: S.of(context).Yes,
+            onPressed: onYesTap,
+          ),
+        ),
+        context.smallButton(
+          data: BaseUiKitButtonData(
+            text: S.current.Cancel,
+            onPressed: onCancelTap,
+            backgroundColor: Colors.transparent,
+          ),
+        )
+      ],
+    ),
+  );
+}
