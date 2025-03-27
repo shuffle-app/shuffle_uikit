607aef1cd8b9b0a6a69dbb75c843429ab1019721
romancores <225000@niuitmo.ru>
2025-03-27 14:38:22 +0700

    fix

--- lib/ui_kit/molecules/tiles/ui_kit_radio_tile.dart
+++ lib/ui_kit/molecules/tiles/ui_kit_radio_tile.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:shuffle_uikit/shuffle_uikit.dart';
 
@@ -16,9 +17,11 @@ class UiKitRadioTile extends StatelessWidget {
       child: Row(
         children: [
           UiKitRadio(selected: selected).paddingOnly(right: SpacingFoundation.horizontalSpacing12),
-          Text(
+          AutoSizeText(
             title,
             style: theme?.boldTextTheme.labelLarge,
+            maxLines: 1,
+            minFontSize: 8,
           ),
         ],
       ).paddingSymmetric(vertical: SpacingFoundation.verticalSpacing12),
